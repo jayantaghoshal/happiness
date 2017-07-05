a4f719a655c03422a7e798019d88f498b6c70148
Erik Elmeke <erik.elmeke@volvocars.com>
2017-07-05 11:07:06 +0200

    Disable commit message validator
    Doesn't work together with zuul because zuul creates
    temporary merge commits with different commit messages.
    
    [No-Jira]
    
    Change-Id: I10b7b4882a3cf75029cf13caae1d5199a661f995

--- tools/ci/jenkins/commit_check.sh
+++ tools/ci/jenkins/commit_check.sh
@@ -4,4 +4,7 @@ source "${SCRIPT_DIR}/common.sh"
 
 # Setup ccache
 USE_CCACHE=1
-python3 ./vendor/volvocars/tools/ci/jenkins/verify_commit_message.py ./vendor/volvocars/
+
+# Disabled commit check because Zuul seems to confuse it by creating temporary
+# merge-commits with "invalid" commit message. 
+#python3 ./vendor/volvocars/tools/ci/jenkins/verify_commit_message.py ./vendor/volvocars/
