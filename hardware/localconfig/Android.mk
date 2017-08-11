1126f42942fefa26c58048203c623c4a20732090
Anders Wallander <anders.wallander@volvocars.com>
2017-08-11 10:40:20 +0200

    Fix default initialization of LocalConfig
    - Use build parameter LOCAL_INIT_RC to deploy init.rc file. Previous
      solution of copying of default LocalConfig during boot didn't work
      with Android O.
    - Conditionally copy the default LocalConfig file. Previously the
      LocalConfig was overwritten unconditionally.
    - In next iteration we will add more tests to verify that an update
      LocalConfig is not overwritten.
    
    Change-Id: Ia66e2cfff83e4b6c457010f6d0a2fd3994b687cd

--- hardware/localconfig/Android.mk
+++ hardware/localconfig/Android.mk
@@ -23,6 +23,9 @@ LOCAL_MODULE := liblocalconfig
 LOCAL_WHOLE_STATIC_LIBRARIES := liblocalconfig_static
 LOCAL_STATIC_LIBRARIES := $(MY_LOCALCONFIG_STATIC_LIBRARIES)
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(MY_LOCALCONFIG_EXPORT_C_INCLUDE_DIRS)
+# We need an init.rc which will initialize with a default
+# localconfig.json if needed.
+LOCAL_INIT_RC := misc/localconfig.rc
 include $(BUILD_SHARED_LIBRARY)
 
 #
@@ -33,15 +36,4 @@ LOCAL_MODULE := localconfig.json
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_OUT)/../vendor/etc
 LOCAL_SRC_FILES := misc/localconfig.json
-include $(BUILD_PREBUILT)
-
-#
-# copy rc file that copies default json file from
-# /vendor/etc/ to /oem_config/
-#
-include $(CLEAR_VARS)
-LOCAL_MODULE := setupfirstfiles.rc
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT)/../vendor/etc/init
-LOCAL_SRC_FILES := misc/setupfirstfiles.rc
 include $(BUILD_PREBUILT)
