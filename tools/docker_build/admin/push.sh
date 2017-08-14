ec0e67a9e6a35edda0ceb9894bf3c9028295a749
Anders Wallander <anders.wallander@volvocars.com>
2017-08-14 17:56:52 +0200

    Add check that image reference is updated before Docker image is pushed.
    Also fix previous commits where I forgot to update the image.ref file.
    
    Change-Id: Iae053573084e16aed031287ee07d471cf8709cac

--- tools/docker_build/admin/push.sh
+++ tools/docker_build/admin/push.sh
@@ -13,24 +13,30 @@ fi
 
 CHANGE_ID=$1
 
-# Now we want to double check that the user is doing the right thing :-)
+# Check that command line change id matches the one in current commit.
 CHANGE_ID_FROM_LOG=$(git log -n 1 | grep Change-Id: | sed -r 's/.*Change-Id: (.*)/\1/')
 if [[ $CHANGE_ID != $CHANGE_ID_FROM_LOG ]]; then
   echo "ERROR! Change Id from latest commit (${CHANGE_ID_FROM_LOG}) did not match the one given on command line (${CHANGE_ID})."
   exit 1
 fi
 
+# Check that command line change id matches image tag.
 LOCAL_NAME=vcc_aosp_build
 TAGGED_CHANGE_ID=$(docker inspect --format "{{ index .Config.Labels \"org.label-schema.vcs-ref\"}}" ${LOCAL_NAME})
-
-# Now we want to tripple check that the user is doing the right thing :-)
 if [[ $CHANGE_ID != $TAGGED_CHANGE_ID ]]; then
   echo "ERROR! Tagged Change Id (${TAGGED_CHANGE_ID}) from image ${LOCAL_NAME} does not match the one given on command line (${CHANGE_ID})."
   exit 1
 fi
 
+# Check that image.ref contains correct path to artifactory image.
 REGISTRY_HOST="swf1.artifactory.cm.volvocars.biz:5002"
 REMOTE_NAME="${REGISTRY_HOST}/test/vcc_aosp_build:${TAGGED_CHANGE_ID}"
+IMAGE_REF_FILE=$SCRIPT_PATH/../image.ref
+IMAGE_REF_FILE_STR=$(<$IMAGE_REF_FILE)
+if [[ $IMAGE_REF_FILE_STR != $REMOTE_NAME ]]; then
+  echo "ERROR! You seemed to forgot to update file ${IMAGE_REF_FILE}. Found \"${IMAGE_REF_FILE_STR}\", but expected \"${REMOTE_NAME}\"."
+  exit 1
+fi
 
 echo "Tagging docker image ${LOCAL_NAME} with ${REMOTE_NAME}"
 docker tag ${LOCAL_NAME} ${REMOTE_NAME}
