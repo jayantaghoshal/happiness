2676e51441eaedd827ee0e36735ad56ecf5866cb
Martin Ejdestig <martin.ejdestig@volvocars.com>
2017-08-21 14:45:40 +0200

    Fix spelling mistake in gate_test.sh
    Change-Id: I521fdd674e6520edaf2aac5b758a9222adce22ec

--- tools/ci/jenkins/gate_test.sh
+++ tools/ci/jenkins/gate_test.sh
@@ -9,7 +9,7 @@ rm -rf out ${OUT_ARCHIVE}
 docker_run artifactory pull ihu_gate_build "${ZUUL_CHANGE_IDS}" ${OUT_ARCHIVE} \
     || die "Could not pull out archive from Artifactory."
 
-tar xvf ${OUT_ARCHIVE} || dir "Could not extract out archive."
+tar xvf ${OUT_ARCHIVE} || die "Could not extract out archive."
 rm ${OUT_ARCHIVE}
 
 # Dummy test case to test whole CI chain down to device
