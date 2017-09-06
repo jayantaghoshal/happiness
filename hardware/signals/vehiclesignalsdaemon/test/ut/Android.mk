f79b5ceb9f03be46a20830291ea290c60ef7fa60
Johan Olsson <johan.olsson.3@volvocars.com>
2017-09-06 10:54:05 +0200

    Unit test for VSD. Ported from the previous Linux/Genivi project to Android platform using the VTS test framework.
    PSS370-3572
    
    Change-Id: If6215d7e01917275bedcf0076fb78edf7bf20956

---
+++ hardware/signals/vehiclesignalsdaemon/test/ut/Android.mk
@@ -0,0 +1,47 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := vsd_unittest
+
+LOCAL_SRC_FILES :=  vsd_unit_test.cpp \
+                    mocks/vsm_inject_mock.cpp \
+                    mocks/vsm_sink_mock.cpp \
+                    mocks/misc_mock.cpp \
+                    ../../src/message_inject.cpp \
+                    ../../src/vsm.cpp
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/stubs \
+                    $(LOCAL_PATH)/mocks \
+                    $(LOCAL_PATH)/../../src 
+                    
+
+LOCAL_SHARED_LIBRARIES :=   liblog \
+                            libcutils \
+                            libdataelements
+
+LOCAL_STATIC_LIBRARIES :=   libgmock                             
+
+LOCAL_CPPFLAGS := -std=c++1z \
+                  -g \
+                  -Werror \
+                  -Wall \
+                  -Wextra \
+                  -Wunused \
+                  -Wno-non-virtual-dtor \
+                  -fexceptions \
+                  -Wno-macro-redefined \
+                  -DUNIT_TEST \
+                  -Wno-missing-braces \
+                  -Wmissing-field-initializers \
+                  -Wunused-variable
+
+# Tag this module as a vts test artifact
+LOCAL_COMPATIBILITY_SUITE := vts
+
+# We only build for 64 bit.
+LOCAL_MULTILIB := 64
+
+include $(BUILD_NATIVE_TEST)
+
+
+
