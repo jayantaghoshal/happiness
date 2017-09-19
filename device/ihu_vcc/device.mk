1c60a05b102a7471b25c498101c586c672ca7652
Patrik Moberg <patrik.moberg.2@volvocars.com>
2017-09-19 16:33:53 +0200

    Initial kernel config for Connectivity
     * Among many things it adds support for arp proxy and IP forwarding
     * which is needed for AUD to TCAM communication through IHU.
    
    Change-Id: I0b6c55ee5513c40e0c9c8f44d3f2fafa517ed449

--- device/ihu_vcc/device.mk
+++ device/ihu_vcc/device.mk
@@ -27,3 +27,10 @@ PRODUCT_COPY_FILES += \
     vendor/volvocars/device/ihu_vcc/init.rc:root/init.${TARGET_PRODUCT}.rc \
     vendor/volvocars/device/ihu_vcc/init.recovery.rc:root/init.recovery.${TARGET_PRODUCT}.rc \
     vendor/volvocars/device/ihu_vcc/ueventd.rc:root/ueventd.${TARGET_PRODUCT}.rc
+
+
+##############################################################
+# VCC required Linux kernel modules
+##############################################################
+KERNEL_DIFFCONFIG += vendor/volvocars/device/ihu_vcc/kernel_configs/vcc_connectivity_diffconfig
+
