c0ac1a2a80d44d0be9bb30b5b0eb20974347e210
Joel Gustafsson <joel.gustafsson@volvocars.com>
2017-09-19 16:19:02 +0200

    Added configuration for engineering build
    Added conditional directives to only build certain tools for userdebug
    and engineering build
    
    Change-Id: I9c3125920801495a91d5d1002054d5c685a81388

--- device/ihu_vcc/device.mk
+++ device/ihu_vcc/device.mk
@@ -28,9 +28,16 @@ PRODUCT_COPY_FILES += \
     vendor/volvocars/device/ihu_vcc/init.recovery.rc:root/init.recovery.${TARGET_PRODUCT}.rc \
     vendor/volvocars/device/ihu_vcc/ueventd.rc:root/ueventd.${TARGET_PRODUCT}.rc
 
+##############################################################
+# Configurations for userdebug and engineering build
+##############################################################
+ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+PRODUCT_PACKAGES += \
+	signaltrace \
+	signalecho
+endif
 
 ##############################################################
 # VCC required Linux kernel modules
 ##############################################################
 KERNEL_DIFFCONFIG += vendor/volvocars/device/ihu_vcc/kernel_configs/vcc_connectivity_diffconfig
-
