6a2fcad4b4436288e24f19b60c141f9e3ec31bf9
Ali Chaudhry <ali.chaudhry@volvocars.com>
2017-09-21 13:43:41 +0200

    Implement IPLM HAL interface
    - Implement IPLM Hal interface.
    - Added make file helper to update all make files for vendor/interface.
    
    [PSS370-3805]
    
    Change-Id: I7b5563493ba8d848e6a138d2672c11527a59525b

---
+++ interfaces/update-makefiles.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# It updates/generates the make files for vendor/volvocars/interfaces
+# Run this file from root of Android project. e.g.
+# cd ~/icup_android/
+# ./vendor/volvocars/interfaces/update-makefiles.sh
+
+source system/tools/hidl/update-makefiles-helper.sh
+
+do_makefiles_update \
+  "vendor.volvocars.hardware:vendor/volvocars/interfaces" \
+  "android.hidl:system/libhidl/transport"
