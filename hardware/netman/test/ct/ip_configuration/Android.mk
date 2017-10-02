7d4767817c71b45d8b3f35c16c48b740050cf917
Philip Werner <philip.werner@volvocars.com>
2017-10-02 07:38:56 +0000

    Fixed ip configuration tests to work with namespaces.
    Most commands in netman_helper.py now run in vcc namespace by default.
    
    Removed meth0 interface tests as the most driver is not operational.
    
    Tidied up parts of the code.
    
    At the moment we are using sleep methods in parts of the tests, as it
    takes a while to fire up netman and to bring up/down interfaces. This
    should change to a more robust way deterministic way.
    
    Change-Id: I838c038d45a75a40810286c24160f254ab2c544f

--- hardware/netman/test/ct/ip_configuration/Android.mk
+++ hardware/netman/test/ct/ip_configuration/Android.mk
@@ -2,6 +2,6 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := VtsNetmandComponentTest
+LOCAL_MODULE := VtsNetmandIpConfigurationComponentTest
 VTS_CONFIG_SRC_DIR := vendor/volvocars/hardware/netman/test/ct/ip_configuration
 include test/vts/tools/build/Android.host_config.mk
