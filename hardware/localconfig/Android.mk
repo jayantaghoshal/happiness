eb5fb3fc624bcf965563ebc9f631b8dd7dd54673
Krzysztof Wesolowski <krzysztof.wesolowski@volvocars.com>
2017-10-04 11:33:23 +0200

    Convert LocalConfig to soong (*.bp)
    This makes dual targeting (host/target) simpler
    
    Also removed leftovers from file based unit tests
    
    Change-Id: I5554e3f432d1eb6196aafdd880e1c4cdb5af1c06

--- hardware/localconfig/Android.mk
+++ hardware/localconfig/Android.mk
@@ -1,61 +1,13 @@
 LOCAL_PATH:= $(call my-dir)
 
-localconfig_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
-localconfig_STATIC_LIBRARIES := libjsoncpp liblog
-localconfig_SRC_FILES := \
-    src/localconfig.cpp \
-    src/local_config_reader_interface.cpp \
-    src/local_config_reader.cpp \
-    src/local_config_file_reader_android_ihu_behavior.cpp
-
-localconfig_CPPFLAGS := -Wno-non-virtual-dtor -fexceptions -Wno-unused-parameter -Wno-macro-redefined
-
-#
-# Static library. Used in shared lib and for unit tests.
-#
-include $(CLEAR_VARS)
-LOCAL_MODULE := liblocalconfig_static
-LOCAL_SRC_FILES := $(localconfig_SRC_FILES)
-LOCAL_CPPFLAGS := $(localconfig_CPPFLAGS)
-LOCAL_C_INCLUDES := $(localconfig_EXPORT_C_INCLUDE_DIRS)
-LOCAL_STATIC_LIBRARIES := $(localconfig_STATIC_LIBRARIES)
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(localconfig_EXPORT_C_INCLUDE_DIRS)
-LOCAL_MULTILIB := 64
-include $(BUILD_STATIC_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := liblocalconfig_static
-LOCAL_SRC_FILES := $(localconfig_SRC_FILES)
-LOCAL_CPPFLAGS := $(localconfig_CPPFLAGS)
-LOCAL_C_INCLUDES := $(localconfig_EXPORT_C_INCLUDE_DIRS)
-LOCAL_STATIC_LIBRARIES := $(localconfig_STATIC_LIBRARIES)
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(localconfig_EXPORT_C_INCLUDE_DIRS)
-LOCAL_MULTILIB := 64
-include $(BUILD_HOST_STATIC_LIBRARY)
-
-#
-# Shared library for deployment to system
-#
-include $(CLEAR_VARS)
-LOCAL_MODULE := liblocalconfig
-LOCAL_VENDOR_MODULE := true
-LOCAL_WHOLE_STATIC_LIBRARIES := liblocalconfig_static
-LOCAL_STATIC_LIBRARIES := $(localconfig_STATIC_LIBRARIES)
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(localconfig_EXPORT_C_INCLUDE_DIRS)
-LOCAL_MULTILIB := 64
-include $(BUILD_SHARED_LIBRARY)
-
-#
 # Copy the default JSON file to /vendor/etc/localconfig
-#
+# Convert to BP when somepne figures that out 
 include $(CLEAR_VARS)
-LOCAL_MODULE := localconfig.json
+LOCAL_MODULE := localconfig_default
 LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_RELATIVE_PATH := localconfig
 LOCAL_SRC_FILES := misc/localconfig.json
 
-# This is probably not needed, please review...
-LOCAL_MULTILIB := 64
 include $(BUILD_PREBUILT)
 include $(call all-makefiles-under,$(LOCAL_PATH))
