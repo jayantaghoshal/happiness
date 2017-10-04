2ed088ec6f89b069c7673273d869366468109aca
Magnus Larsson <magnus.larsson.5@volvocars.com>
2017-10-04 17:34:54 +0200

    Fix problems with ihu_gate_build
    - Fix problems in ihu_gate_build were manifest repo could get stuck in rebase state
      Problems seen when on master branch doing reset --hard origin/ZUUL_BRANCH (and ZUUL_BRANCH is not master)
    
    - Small fix in "ihu_image_build": Remove unnecessary check
    
    Change-Id: I88655e549044ba34e779003a8c9b46394d61488d

--- tools/ci/jenkins/jenkins_job_builder/jobs/bootstrap_ihu_image_build.sh
+++ tools/ci/jenkins/jenkins_job_builder/jobs/bootstrap_ihu_image_build.sh
@@ -3,9 +3,7 @@
 ##
 # Repo sync
 #
-if [[ ! -d .repo ]]; then
-    docker_run "repo init -u ${GIT_URL} -b ${GIT_COMMIT}" || die "repo init failed"
-fi
+docker_run "repo init -u ${GIT_URL} -b ${GIT_COMMIT}" || die "repo init failed"
 docker_run "repo sync --no-clone-bundle --current-branch -q -j8" || die "repo sync failed"
 
 ##
