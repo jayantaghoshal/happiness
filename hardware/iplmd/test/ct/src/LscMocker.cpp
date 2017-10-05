91ba411c96c6b416de68961848056f05e0305fa0
Tobias Öhrström <tobias.ohrstrom@volvocars.com>
2017-10-05 14:13:30 +0200

    Created iplmd component test skeleton
    GTestCase inits LscMocker, which is a class that registers to
    iplmservice.
    
    Testcases
    -RegisterUnRegisterLSCTest
    -WakeUpOnFlexRayTest
    
    Change-Id: I57fb86737c890484701795ae17e5b2236048fa6d

---
+++ hardware/iplmd/test/ct/src/LscMocker.cpp
@@ -0,0 +1,39 @@
+#include "LscMocker.h"
+
+LscMocker::LscMocker(){
+  try{
+    ALOGD("IIplm getService!");
+    iplmservice = IIplm::getService();
+  }
+  catch (const std::runtime_error &e){
+    ALOGE("Couldnt getService : Exception thrown: %s", e.what());
+  }
+}
+void LscMocker::RegisterLSC(std::string LSCName){
+  if(iplmservice != nullptr)
+    iplmservice->registerService(LSCName,this);
+}
+void LscMocker::UnregisterLSC(std::string LSCName){
+  if(iplmservice != nullptr)
+    iplmservice->unregisterService(LSCName);
+}
+void LscMocker::ReleaseResourceGroup(const hidl_string& lscName, ResourceGroup _rg){
+  if(iplmservice != nullptr)
+    iplmservice->releaseResourceGroup(lscName,_rg);
+}
+void LscMocker::RequestResourceGroup(const hidl_string& lscName, ResourceGroup _rg, ResourceGroupPrio _prio){
+  if(iplmservice != nullptr)
+    iplmservice->requestResourceGroup(lscName,_rg,_prio);
+}
+
+// Methods from ::vendor::volvocars::hardware::iplm::V1_0::IIplmCallback follow.
+Return<void> LscMocker::onResourceGroupStatus(ResourceGroup resourceGroup, ResourceGroupStatus resourceGroupStatus) {
+  if(onResourceGroupStatusCallback != nullptr)
+    onResourceGroupStatusCallback(resourceGroup,resourceGroupStatus);
+  return Void();
+}
+Return<void> LscMocker::onNodeStatus(Ecu ecuType, bool ecuStatus) {
+  if(onNodeStatusCallback != nullptr)
+    onNodeStatusCallback(ecuType, ecuStatus);
+  return Void();
+}
