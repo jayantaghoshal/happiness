c953bfeec0174e1bea9280e85c166ab1d9a9fb2d
Andreas Bengtsson <andreas.bengtsson.3@volvocars.com>
2017-10-16 14:42:50 +0200

    Break out ipcommandbus to separate lib (libipcommandbus)
    - Use new lib from IpcbD
    
    Change-Id: I37e0394aac49da4a4d5830fe8ad381964064ec04

--- hardware/ipcbd/Android.mk
+++ hardware/ipcbd/Android.mk
@@ -21,23 +21,6 @@ LOCAL_SRC_FILES := \
 # this will however disable these warnings for all .c and .cpp file which is not what we want.
 # How can we disable these warnings only for these .c files??
 
-# libipcommandbus - not built as static lib due to dependence problems in Android .mk files
-LOCAL_SRC_FILES += \
-    libipcommandbus/src/IpCmdTypes.cpp \
-    libipcommandbus/src/Message.cpp \
-    libipcommandbus/src/MessageDispatcher.cpp \
-    libipcommandbus/src/Pdu.cpp \
-    libipcommandbus/src/socket.cpp \
-    libipcommandbus/src/UdpSocket.cpp \
-    libipcommandbus/src/TcpSocket.cpp \
-    libipcommandbus/src/vcc_pdu_header.cpp \
-    libipcommandbus/src/net_serializer.cpp \
-    libipcommandbus/src/net_deserializer.cpp \
-    libipcommandbus/src/TimeoutInfo.cpp \
-    libipcommandbus/src/TransportServices.cpp \
-    libipcommandbus/src/local_config_parameters.cpp
-#    libipcommandbus/src/shutdown_client.cpp
-
 # TODO check if libhwbinder is the correct one to use here
 #LOCAL_CPPFLAGS := -fexceptions -frtti -DLOG_NDEBUG=0 -g
 LOCAL_CPPFLAGS := -fexceptions -DLOG_NDEBUG=0 -g
@@ -51,12 +34,9 @@ LOCAL_SHARED_LIBRARIES += \
     libhidltransport \
     libhardware \
     libhwbinder \
+    libipcommandbus \
     vendor.volvocars.hardware.common@1.0 \
     vendor.volvocars.hardware.vehiclecom@1.0
-
-LOCAL_C_INCLUDES := \
-    $(LOCAL_PATH)/ipcbd/src \
-    $(LOCAL_PATH)/libipcommandbus/include
 
 LOCAL_MULTILIB := 64
 include $(BUILD_EXECUTABLE)
