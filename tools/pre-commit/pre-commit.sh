335a13c7e0168a290c6c7cb27bd0571d8a69d606
Krzysztof Wesolowski <krzysztof.wesolowski@volvocars.com>
2017-10-23 19:11:34 +0200

    Add pre-commit hooks for formatting and static analysis
    This will reformat all modified files canonically
    and stage them. If files were not properly formatted
    they would be staged (this can break partial commits
    if someone has not configured ide to use clang)
    
    Change-Id: I14234f909a6e27721d2a7130e252c23583be5fa2

---
+++ tools/pre-commit/pre-commit.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+SCRIPT_DIR=$(cd "$(dirname "$(readlink -f "$0")")"; pwd)
+REPO_ROOT_DIR=$(readlink -f "${SCRIPT_DIR}"/../../../..)
+
+PRE_COMMIT_SCRIPT="${SCRIPT_DIR}/pre-commit_in-docker.sh"
+echo "Running ${PRE_COMMIT_SCRIPT} in docker..."
+"${REPO_ROOT_DIR}/vendor/volvocars/tools/docker_build/run.sh" "${PRE_COMMIT_SCRIPT}"
