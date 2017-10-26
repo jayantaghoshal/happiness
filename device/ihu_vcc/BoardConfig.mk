12a7ef0fffbcb3e67b436239175d87ca641da433
Fredrik de Groot <fredrik.de.groot@volvocars.com>
2017-10-26 07:43:48 +0200

    Add permissive setting due to selinux problems
    Change-Id: I1afcd6db79212af5e55d7526f3a01bcb462ad1e3

--- device/ihu_vcc/BoardConfig.mk
+++ device/ihu_vcc/BoardConfig.mk
@@ -1,6 +1,8 @@
 # Inherit from Delphi's BoardConfig
 include device/delphi/volvoihu/ihu_abl_car/BoardConfig.mk
 
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+
 BOARD_SEPOLICY_DIRS += \
 	vendor/volvocars/device/ihu_vcc/sepolicy
 
