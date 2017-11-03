96b71bc9c64c1c0ebaea7f83863a31ad215d2a0c
Philip Werner <philip.werner@volvocars.com>
2017-11-03 07:14:45 +0000

    Added component ping test between IHU and AUD
    Change-Id: Iab1d24505ef6cf0d404e933e0d2b5e89ce935dd6

---
+++ hardware/netman/test/ct/helpers/execution_context.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import logging
+
+from os.path import join
+
+from vts.runners.host import const
+
+import re
+
+class ExecutionContext(object):
+
+    def __init__(self, terminal):
+        self.terminal = terminal
+
+    def execute(self, cmd):
+        logging.info("execute cmd: " + cmd)
+        return self.terminal.Execute(cmd)
+
+class NamespaceExecutionContext(ExecutionContext):
+
+    def __init__(self, namespace, terminal):
+        ExecutionContext.__init__(self, terminal)
+        self.namespace = namespace
+
+    def execute(self, cmd):
+        formatted_cmd = self._format_cmd_line(cmd)
+        logging.info("formatted cmd: " + formatted_cmd)
+        return self.terminal.Execute(formatted_cmd)
+
+    def _format_cmd_line(self, cmd):
+        return " ".join(["ip", "netns", "exec"] + [self.namespace] + [cmd])
---
+++ hardware/netman/test/ct/most_interface/VtsMostInterfaceComponentTest.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import logging
+
+from vts.runners.host import asserts
+from vts.runners.host import base_test
+from vts.runners.host import test_runner
+from vts.utils.python.controllers import android_device
+
+from volvocars.hardware.netman.test.ct.helpers.execution_context import (ExecutionContext,
+                                                                         NamespaceExecutionContext)
+from volvocars.hardware.netman.test.ct.helpers.network_helpers import PingTest
+
+class MostInterfaceComponentTest(base_test.BaseTestClass):
+
+    def setUpClass(self):
+        self.dut = self.registerController(android_device)[0]
+
+    def setUp(self):
+        self.terminal = None
+        self.terminal = self._invoke_terminal()
+
+    ## ----------------------------------------
+    ## --/ MOST (meth0) Interface Ip Configuration Tests /--
+    ## ----------------------------------------
+
+    def test_WhenMeth0Configured_ShouldEnablePingToAUD(self):
+        try:
+            # Arrange
+            ping_test = PingTest(NamespaceExecutionContext("vcc", self.terminal))
+            aud_ip_address = "198.18.2.18"
+
+            # Act
+            ping_test.ping(aud_ip_address)
+
+            # Assert
+            asserts.assertTrue(ping_test.is_destination_reachable, "AUD unit not reachable by ping")
+        except Exception as e:
+            asserts.fail("Exception thrown: {}".format(e))
+
+    def _invoke_terminal(self, name="test_terminal"):
+        self.dut.shell.InvokeTerminal(name)
+        return getattr(self.dut.shell, name)
+
+if __name__ == "__main__":
+    test_runner.main()
