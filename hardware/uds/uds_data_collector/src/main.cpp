6c75405c5b90e62a4d6e1cfa27fda5549b8b2d7a
Krzysztof Wesolowski <krzysztof.wesolowski@volvocars.com>
2017-11-17 13:59:05 +0100

    Fix uds data_collector to use android::sp for lifetime management
    - this is relevant only in the event of failed registrations,
    but is generally advisable for consistency.
    - also fixed ignored return values.
    
    Change-Id: I8a82ae962bd5263d2efcac0463dfb0f853a3e2f8

--- hardware/uds/uds_data_collector/src/main.cpp
+++ hardware/uds/uds_data_collector/src/main.cpp
@@ -2,20 +2,36 @@
 
 #include "UdsDataCollector.h"
 
+#include <cutils/log.h>
+#undef LOG_TAG
+#define LOG_TAG "uds-collector"
+
+using android::sp;
 using android::hardware::configureRpcThreadpool;
 using android::hardware::joinRpcThreadpool;
 
 // TODO(krzysztof.wesolowski@volvocars.com) subscribe as DiagKeys handler,
 // translate them back to DIDs and ask registered providers.
 int main() {
-    configureRpcThreadpool(1, true);
-    auto data_collector = std::make_unique<UdsDataCollector>();
+    configureRpcThreadpool(1, true /*caller thread would join the threadpool */);
+
+    sp<UdsDataCollector> data_collector = new UdsDataCollector();
 
     auto* collector = static_cast<IUdsDataCollector*>(data_collector.get());
     auto* aggregated_provider = static_cast<IUdsDataProvider*>(data_collector.get());
 
-    collector->registerAsService();
-    aggregated_provider->registerAsService("test-point");
+    auto collector_status = collector->registerAsService();
+    if (collector_status != android::NO_ERROR) {
+        ALOGE("Registration as a collector service failed, error = %i", collector_status);
+        return collector_status;
+    }
+
+    auto test_point_status = aggregated_provider->registerAsService("test-point");
+
+    if (test_point_status != android::NO_ERROR) {
+        ALOGE("Registration as a provider service (test-point) failed, error = %i", test_point_status);
+        return test_point_status;
+    }
 
     joinRpcThreadpool();
 }
