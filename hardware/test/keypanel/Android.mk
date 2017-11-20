716014547c77608f4a867baf82fdd6361355f1db
Tobias Ohrstrom <tobias.ohrstrom@volvocars.com>
2017-11-20 10:29:47 +0100

    Add keypanel test
    Change-Id: Ib3d67b148f35a7d05cd83a5753f2730c52bd0e2b

---
+++ hardware/test/keypanel/Android.mk
@@ -0,0 +1,18 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_PACKAGE_NAME := VtsKeyPanelTestApk
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_DATA_APPS)
+ANDROID_PRODUCT_OUT:= $(TARGET_OUT_DATA_APPS)
+
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+LOCAL_PROGUARD_ENABLED := disabled
+LOCAL_MULTILIB := 64
+include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := VtsKeyPanelTest
+VTS_CONFIG_SRC_DIR := vendor/volvocars/hardware/test/keypanel/
+include test/vts/tools/build/Android.host_config.mk
