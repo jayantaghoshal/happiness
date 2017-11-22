23e04e3e5ede91ddc02853180320b6d25f6e4fe4
Krzysztof Wesolowski <krzysztof.wesolowski@volvocars.com>
2017-11-22 15:29:45 +0100

    Use BSP fastboot.sh for partitions
    + fix detection of succesful start of ABL
    + fix name of the function starting ABL
    
    Change-Id: Ia15afd89b61a9cb677d8e7331887d91fd15e8ff3

--- tools/ci/shipit/ihu_update.py
+++ tools/ci/shipit/ihu_update.py
@@ -22,8 +22,8 @@ from shipit.process_tools import check_output_logged
 logger = logging.getLogger(__name__)
 
 
-def boot_mp_to_elk(vip_serial: recording_serial.RecordingSerial):
-    logging.info("Boot into ELK")
+def boot_mp_to_abl_cmdline(vip_serial: recording_serial.RecordingSerial):
+    logging.info("Boot into ABL CMD line")
     vip_serial.writeline("gpio 1.10 0 o")
     vip_serial.writeline("gpio 8.10 0 o")
     vip_serial.writeline("gpio 1.10 1 o")
@@ -75,7 +75,6 @@ def wait_for_dmesg_timestamp(s: recording_serial.RecordingSerial, device_timesta
 
 def flash_image(port_mapping: PortMapping,
                 product: str,
-                hardware: str,
                 build_out_dir: str):
     adb_executable = os.path.join(
         build_out_dir, "host", "linux-x86", "bin", "adb")
@@ -99,13 +98,13 @@ def flash_image(port_mapping: PortMapping,
     try:
         ihu_serials.vip.writeline("sm alw 1")
         expect_line(ihu_serials.vip, ".*SysM- Always_On: 1.*", 15)
-        logger.info("Set boot mode to ELK")
-        boot_mp_to_elk(ihu_serials.vip)
-        expect_line(ihu_serials.mp, "Android", 30,
+        logger.info("Starting ABL command line")
+        boot_mp_to_abl_cmdline(ihu_serials.vip)
+        expect_line(ihu_serials.mp, "abl-APL.*", 30,
                     "Is the MP UART connected? Or do you have the TTY open already?")
         expect_line(ihu_serials.mp, "auto-boot ...", 30)
         expect_line(ihu_serials.mp, ">>>.*", 30)
-        logger.info("ELK confirmed, boot elk to fastboot")
+        logger.info("ABL command line confirmed, boot into fastboot")
         ihu_serials.mp.writeline("boot elk")
         expect_line(ihu_serials.mp, "==> jump to image.*", 30)
         expect_line(ihu_serials.mp,
@@ -128,39 +127,17 @@ def flash_image(port_mapping: PortMapping,
             raise Exception(
                 "No Fastboot device found. Did you forget to connect host with target?")
 
-        logger.info("Assigning partition table")
-        img_path = os.path.join(
-            build_out_dir, "target", "product", product, "fast_flashfiles", "gpt_" + hardware + ".bin")
-        output = check_output_logged([fastboot_executable,
-                                      "flash", "gpt", img_path]).decode().strip(" \n\r\t")
+        bsp_provided_flashfiles_path = os.path.join(build_out_dir,
+                                                    "target",
+                                                    "product",
+                                                    product,
+                                                    "fast_flashfiles")
 
-        logger.info("Flashing bootloader partition")
-        bootloader_path = os.path.join(
-            build_out_dir, "target", "product", product, "fast_flashfiles", "bootloader_" + hardware)
-        output = check_output_logged([fastboot_executable,
-                                      "flash", "bootloader", bootloader_path]).decode().strip(" \n\r\t")
+        logger.info("Running fastboot.sh inside " + bsp_provided_flashfiles_path)
+        check_output_logged(['bash', 'fastboot.sh'],
+                            cwd=bsp_provided_flashfiles_path).decode().strip(" \n\r\t")
 
-        for parition_to_flash in ["boot", "multiboot", "system", "vendor", "recovery"]:
-            logger.info("Flashing " + parition_to_flash +  " partition")
-            img_path = os.path.join(
-                build_out_dir, "target", "product", product, "fast_flashfiles", parition_to_flash + ".img")
-            output = check_output_logged([fastboot_executable,
-                                          "flash", parition_to_flash, img_path]).decode().strip(" \n\r\t")
-
-        for partition_to_erase in ["data", "misc", "metadata", "persistent"]:
-            logger.info("Erasing " + partition_to_erase +  " partition")
-            output = check_output_logged([fastboot_executable,
-                                          "erase", partition_to_erase]).decode().strip(" \n\r\t")
-
-        for partition_to_format in ["config", "cache"]:
-            logger.info("Formating " + partition_to_format +  " partition")
-            output = check_output_logged([fastboot_executable,
-                                          "format", partition_to_format]).decode().strip(" \n\r\t")
-
-        output = check_output_logged([fastboot_executable,
-                                      "reboot"]).decode().strip(" \n\r\t")
-
-        boot_mp_to_android(ihu_serials.vip)
+        # boot_mp_to_android(ihu_serials.vip)
 
         logging.info("Wait for device to enter device-mode via ADB")
         output = check_output_logged([adb_executable,
@@ -195,7 +172,7 @@ def main():
     parser = argparse.ArgumentParser(
         description="Update an IHU using fastboot.")
     parser.add_argument("--product", default="ihu_vcc", help="Product")
-    parser.add_argument("--hardware", default="ihu_6_0_HIGH", help="Hardware")
+    parser.add_argument("--hardware", default="", help="Hardware")
     parser.add_argument("--aosp_root_dir", required=True,
                         help="Repo root directory")
     parser.add_argument(
@@ -217,10 +194,12 @@ def main():
 
     port_mapping = PortMapping(parsed_args.vip_port, parsed_args.mp_port)
     product = parsed_args.product
-    hardware = parsed_args.hardware
-    flash_image(port_mapping, product, hardware, build_out_dir)
-    logging.info("Flash completed")
 
+    if parsed_args.hardware != "":
+        logger.warning("Hardware is auto detected, stop passing it manually")
+
+    flash_image(port_mapping, product, build_out_dir)
+    logging.info("Flash completed")
 
 
 if __name__ == "__main__":
