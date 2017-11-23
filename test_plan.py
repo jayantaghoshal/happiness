227c3373f4823ec17f1fc86d2241dc8690fbac98
Martin Hansson <martin.hansson@volvocars.com>
2017-11-23 07:19:08 +0100

    Enabled testASN1 tests
    Change-Id: Ibc633c186482a2af2b036106c7c8bd87151906e3

--- test_plan.py
+++ test_plan.py
@@ -45,11 +45,6 @@ test_plan_hourly = [
              jira_issue="",
              deadline="2018-01-21"
     ),
-    Disabled(VTSTest("vendor/volvocars/hardware/infotainmentIpBus/test/ut",            standard_caps),
-             reason="Dead code in repo",
-             jira_issue="",
-             deadline="2017-12-04"
-    ),
     Disabled(VTSTest("vendor/volvocars/hardware/dim/test/ct/apix_gate",       standard_caps),
              reason="APIX driver not yet integrated on Master",
              jira_issue="",
@@ -60,6 +55,7 @@ test_plan_hourly = [
     VTSTest("vendor/volvocars/hardware/ipcbd/test/ct",                          standard_caps),
     VTSTest("vendor/volvocars/hardware/test/iplm",                              standard_caps),
     VTSTest("vendor/volvocars/hardware/test/gnss",                              standard_caps),
+    VTSTest("vendor/volvocars/hardware/infotainmentIpBus/test/ut",              standard_caps),
     VTSTest("vendor/volvocars/hardware/signals/vehiclesignalsdaemon/test/ut",   standard_caps),
     Disabled(VTSTest("vendor/volvocars/hardware/netman/test/ct/boot",       standard_caps),
              reason="Reboot is still unstable",
