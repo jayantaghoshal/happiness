a02ec4d8a7f65a156cca32e7421328d8fb283267
Krzysztof Wesolowski <krzysztof.wesolowski@volvocars.com>
2017-12-07 20:48:07 +0100

    Add automatic handling of copyright headers
     - header removal
     - header insertion
     - header verification
    
    Change-Id: I0d6a18909f100df358cca3b9227ab11afc7b7bff

--- tools/ci/licensing-components/main.py
+++ tools/ci/licensing-components/main.py
@@ -3,6 +3,10 @@ licensing-components
 
 Usage:
   licensing-components software-bom <root> <output_path>
+  licensing-components list-copyright-headers <files>...
+  licensing-components fix-copyright-headers <files>...
+  licensing-components fix-all-copyright-headers <directory>
+  licensing-components verify-copyright-headers <files>...
   licensing-components -h | --help
   licensing-components --version
 
@@ -19,15 +23,15 @@ Help:
 
   It gathers data from .vcc-component.yaml files, and uses it to label
   the files, create software bom etc.
-
-  If we need to create file headers it will also automate this part.
 """
 import os
 
 from jinja2 import Environment, PackageLoader, select_autoescape
 
 from licensingcomponents.common import descriptions, source, validation
+from licensingcomponents.copyrightheader.copyrightheader import filter_ignored, should_filepath_be_ignored
 from licensingcomponents.docopt_dispatch import dispatch
+from licensingcomponents.copyrightheader import copyrightheader
 
 env = Environment(
     loader=PackageLoader('licensingcomponents'),
@@ -54,6 +58,65 @@ def software_bom(root, output_path, **kwargs):
                                         sources=source_directories,
                                         not_accounted_sources=validation_result.not_accounted_sources
                                         ))
+
+
+@dispatch.on('list-copyright-headers')
+def list_copyright_headers(files, **kwargs):
+    for f in files:
+        if should_filepath_be_ignored(f):
+            print("Ignoring file {}".format(f))
+            continue
+
+        copyrightheader.list_headers_and_filenames(f)
+    pass
+
+
+@dispatch.on('fix-copyright-headers')
+def fix_copyright_headers(file_or_files_list, **kwargs):
+    if isinstance(file_or_files_list, str):
+        files = [file_or_files_list]
+    else:
+        files = file_or_files_list
+
+    for f in filter_ignored(files):
+        assert os.path.isfile(f)
+
+        fixed, original = copyrightheader.get_contents_with_header_applied(f)
+        if fixed != original:
+            with(open(f, 'w', encoding="utf-8")) as target:
+                target.write(fixed)
+            print("Fixed header in file {0}".format(f))
+    pass
+
+
+def collect_supported_files(directory):
+    for entry in os.scandir(directory):
+        if entry.is_dir():
+            yield from collect_supported_files(entry.path)
+
+        if entry.is_file() and os.path.splitext(entry.path)[1] in copyrightheader.get_known_languages_extensions():
+            yield entry.path
+    pass
+
+
+def collect_copyrightable_files(directory):
+    yield from filter_ignored(collect_supported_files(directory))
+
+
+@dispatch.on('fix-all-copyright-headers')
+def fix_all_copyright_headers(directory, **kwargs):
+    for path in collect_copyrightable_files(directory):
+        fix_copyright_headers(path)
+
+
+@dispatch.on('verify-copyright-headers')
+def verify_copyright_headers(files, **kwargs):
+    for f in filter_ignored(files):
+
+        fixed, original = copyrightheader.get_contents_with_header_applied(f)
+        if fixed != original:
+            raise Exception("Copyright header invalid in file {0}".format(f))
+    pass
 
 
 if __name__ == "__main__" and __package__ is None:
