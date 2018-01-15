d2a7130239a3a69d44d553371f31e24070e684ba
Krzysztof Wesolowski <krzysztof.wesolowski@volvocars.com>
2018-01-15 14:48:16 +0100

    Fix pre-commit behavior when running from docker
    Also allow to use git review from inside
    
    [PSS370-12870]
    
    Change-Id: Ibb8e9889431c54c72410956f61b3c44677a48b4d

--- tools/pre-commit/pre-commit.sh
+++ tools/pre-commit/pre-commit.sh
@@ -8,5 +8,13 @@ SCRIPT_DIR=$(cd "$(dirname "$(readlink -f "$0")")"; pwd)
 REPO_ROOT_DIR=$(readlink -f "${SCRIPT_DIR}"/../../../..)
 
 PRE_COMMIT_SCRIPT="${SCRIPT_DIR}/pre-commit_in-docker.sh"
-echo "Running ${PRE_COMMIT_SCRIPT} in docker..."
-"${REPO_ROOT_DIR}/vendor/volvocars/tools/docker_build/run.sh" "${PRE_COMMIT_SCRIPT}" < /dev/null
+
+if [ "$HOSTNAME" = "aic-docker" ]; then
+    echo "Already in aic-docker, running ${PRE_COMMIT_SCRIPT}..."
+    "${PRE_COMMIT_SCRIPT}"
+    exit 0
+else
+    echo "Launching new aic-docker, running ${PRE_COMMIT_SCRIPT}..."
+    "${REPO_ROOT_DIR}/vendor/volvocars/tools/docker_build/run.sh" "${PRE_COMMIT_SCRIPT}" < /dev/null
+    exit 0
+fi
