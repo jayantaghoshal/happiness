418162ef3d074fef70ad5bad2a66b1355657062b
Kenneth Karlsson <kenneth.karlsson.2@volvocars.com>
2018-01-19 09:16:33 +0100

    Update jobs to support multiple repos in CI
    Change-Id: Ib7a37bfa2806e4735e207e16b184efa3f2be755a

--- tools/ci/jenkins/jenkins_job_builder/jobs/bootstrap_common_commit_triggered.sh
+++ tools/ci/jenkins/jenkins_job_builder/jobs/bootstrap_common_commit_triggered.sh
@@ -101,10 +101,10 @@ bootstrap_docker_run "repo forall -c 'git reset --hard ; git clean -fdx'"
 ## Download the commit to check (for vendor/volvocars-repo)
 #
 # zuul-cloner implicity uses other environment variables as well, such as ZUUL_REF.
-bootstrap_docker_run "GIT_SSH=$HOME/zuul_ssh_wrapper.sh zuul-cloner -v ${ZUUL_URL} vendor/volvocars"
+bootstrap_docker_run "GIT_SSH=$HOME/zuul_ssh_wrapper.sh zuul-cloner -v ${ZUUL_URL} ${ZUUL_PROJECT}"
 
-if [ "$(git -C vendor/volvocars rev-parse HEAD)" != "$ZUUL_COMMIT" ]; then
-    die "zuul-cloner failed to checkout commit $ZUUL_COMMIT in vendor/volvocars."
+if [ "$(git -C " $ZUUL_PROJECT" rev-parse HEAD)" != "$ZUUL_COMMIT" ]; then
+    die "zuul-cloner failed to checkout commit $ZUUL_COMMIT in $ZUUL_PROJECT"
 fi
 
 ################################################################################################
