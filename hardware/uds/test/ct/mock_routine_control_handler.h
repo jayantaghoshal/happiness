3979f6f491a97c748dfe5bf4cd875f134b9a566e
Krzysztof Wesolowski <krzysztof.wesolowski@volvocars.com>
2018-01-19 14:10:43 +0100

    Improve test coverage for uds data collector
    Change-Id: I58b5ba2f7c417fbf473e2673d1c3621a5b5ee91c

---
+++ hardware/uds/test/ct/mock_routine_control_handler.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2017 Volvo Car Corporation
+ * This file is covered by LICENSE file in the root of this project
+ */
+
+#pragma once
+
+#include <gmock/gmock.h>
+#include <vendor/volvocars/hardware/uds/1.0/IRoutineControlHandler.h>
+
+namespace vendor {
+namespace volvocars {
+namespace hardware {
+namespace uds {
+namespace V1_0 {
+namespace mocks {
+
+struct MockRoutineControlHandler : public IRoutineControlHandler {
+    MOCK_CONST_METHOD1(StartAsynchronousRoutine,
+                       RoutineExecutionStartResponse(const RoutineExecutionStartRequest& request));
+    MOCK_CONST_METHOD1(StopAsynchronousRoutine, RoutineExecutionStopResponse(const RoutineExecutionStopRequest&));
+    MOCK_CONST_METHOD1(GetAsynchronousRoutineResults,
+                       RoutineExecutionResultsResponse(const RoutineExecutionResultsRequest&));
+
+    ::android::hardware::Return<void> startAsynchronousRoutine(const RoutineExecutionStartRequest& request,
+                                                               startAsynchronousRoutine_cb _hidl_cb) override {
+        _hidl_cb(StartAsynchronousRoutine(request));
+        return ::android::hardware::Void();
+    }
+
+    ::android::hardware::Return<void> stopAsynchronousRoutine(const RoutineExecutionStopRequest& request,
+                                                              stopAsynchronousRoutine_cb _hidl_cb) override {
+        _hidl_cb(StopAsynchronousRoutine(request));
+        return ::android::hardware::Void();
+    }
+
+    ::android::hardware::Return<void> getAsynchronousRoutineResults(
+            const RoutineExecutionResultsRequest& request, getAsynchronousRoutineResults_cb _hidl_cb) override {
+        _hidl_cb(GetAsynchronousRoutineResults(request));
+        return ::android::hardware::Void();
+    }
+};
+
+}  // namespace mocks
+}  // namespace V1_0
+}  // namespace uds
+}  // namespace hardware
+}  // namespace volvocars
+}  // namespace vendor
--- hardware/uds/test/ct/uds_collector_proper_forwarding_test.cpp
+++ hardware/uds/test/ct/uds_collector_proper_forwarding_test.cpp
@@ -11,6 +11,7 @@
 #include <iostream>
 
 #include "mock_data_handler.h"
+#include "mock_routine_control_handler.h"
 
 using namespace ::vendor::volvocars::hardware::uds::V1_0;
 using namespace ::vendor::volvocars::hardware::uds::V1_0::mocks;
@@ -19,11 +20,14 @@ using ::android::sp;
 using ::android::hardware::Void;
 using ::android::hardware::Return;
 
+constexpr uint16_t sample_routine_id = 0x0003u;
+
 struct UdsCollectorProperForwardingTest : public ::testing::Test {
   public:
     void SetUp() override {
         provider1 = new ::testing::StrictMock<MockDataHandler>();
         provider2 = new ::testing::StrictMock<MockDataHandler>();
+        routine_handler = new ::testing::StrictMock<MockRoutineControlHandler>();
         collector = IDataCollector::getService();
         test_point = IDataCollectorTestPoint::getService();
 
@@ -32,6 +36,7 @@ struct UdsCollectorProperForwardingTest : public ::testing::Test {
 
         h1 = collector->registerDataHandler(provider1, {0x0001});
         h2 = collector->registerDataHandler(provider2, {0x0002});
+        h3 = collector->registerRoutineHandler(routine_handler, {sample_routine_id});
     }
 
     void TearDown() override {
@@ -39,15 +44,19 @@ struct UdsCollectorProperForwardingTest : public ::testing::Test {
         EXPECT_TRUE(ret1);
         auto ret2 = collector->unregister(h2);
         EXPECT_TRUE(ret2);
+        auto ret3 = collector->unregister(h3);
+        EXPECT_TRUE(ret3);
     }
 
     sp<MockDataHandler> provider1;
     sp<MockDataHandler> provider2;
+    sp<MockRoutineControlHandler> routine_handler;
     sp<IDataCollectorTestPoint> test_point;
     sp<IDataCollector> collector;
 
     SubscriptionHandle h1;
     SubscriptionHandle h2;
+    SubscriptionHandle h3;
 };
 
 TEST_F(UdsCollectorProperForwardingTest, UnknownDidReturnNotSupported) {
@@ -86,4 +95,50 @@ TEST_F(UdsCollectorProperForwardingTest, ForwardingCallToProviderTwo) {
         EXPECT_THAT((std::vector<uint8_t>)didread.data, ::testing::ElementsAre(0x02));
     });
     EXPECT_TRUE(ret.isOk());
+}
+
+TEST_F(UdsCollectorProperForwardingTest, ForwardingRoutineHandlerSequence) {
+    ::testing::InSequence is;
+
+    RoutineExecutionStartRequest re_start_request{sample_routine_id, {}};
+    RoutineExecutionStartResponse re_start_response{RoutineControlStatusCode::SUCCESS, {0x01u}};
+
+    RoutineExecutionStopRequest re_stop_request{sample_routine_id, {0x02u}};
+    RoutineExecutionStopResponse re_stop_response{RoutineControlStatusCode::SUCCESS, {0x03u}};
+
+    RoutineExecutionResultsRequest re_get_request{sample_routine_id};
+    RoutineExecutionResultsResponse re_get_response{RoutineControlStatusCode::SUCCESS, {0x04u, 0x05u}};
+
+    EXPECT_CALL(*routine_handler, StartAsynchronousRoutine(re_start_request))
+            .WillOnce(::testing::Return(re_start_response));
+
+    EXPECT_CALL(*routine_handler, StopAsynchronousRoutine(re_stop_request))
+            .WillOnce(::testing::Return(re_stop_response));
+
+    EXPECT_CALL(*routine_handler, GetAsynchronousRoutineResults(re_get_request))
+            .WillOnce(::testing::Return(re_get_response));
+
+    auto ret_start = test_point->startAsynchronousRoutine(
+            re_start_request,  //
+            [re_start_response](const RoutineExecutionStartResponse& actual_response) {
+                EXPECT_THAT(actual_response, re_start_response);
+            });
+
+    ASSERT_TRUE(ret_start.isOk());
+
+    auto ret_stop = test_point->stopAsynchronousRoutine(
+            re_stop_request,  //
+            [re_stop_response](const RoutineExecutionStopResponse& actual_response) {
+                EXPECT_THAT(actual_response, re_stop_response);
+            });
+
+    ASSERT_TRUE(ret_stop.isOk());
+
+    auto ret_get_results = test_point->getAsynchronousRoutineResults(
+            re_get_request,  //
+            [re_get_response](const RoutineExecutionResultsResponse& actual_response) {
+                EXPECT_THAT(actual_response, re_get_response);
+            });
+
+    ASSERT_TRUE(ret_get_results.isOk());
 }
