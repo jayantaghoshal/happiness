eb4f756d51c2cbbb2f96468ffadb9b31cae4852e
lveeraku <logesh.veera.kumar.kalyanasundaram@volvocars.com>
2018-01-25 16:36:37 +0100

    Update the nuc smoke test
    Change-Id: I8c062d0fc191d68347bce4439869382cfa532b05
    note: jenkin job updated using GUI

--- tools/ci/jenkins/nuc_smoke_test.sh
+++ tools/ci/jenkins/nuc_smoke_test.sh
@@ -7,12 +7,33 @@ set -ex
 
 SCRIPT_DIR=$(cd "$(dirname "$(readlink -f "$0")")"; pwd)
 source "${SCRIPT_DIR}/common.sh"
+REPO_ROOT_DIR=$(readlink -f "${SCRIPT_DIR}"/../../../../..)
 
-# Test IHU Update
-"${SCRIPT_DIR}/hourly_test_common.sh"
+cd "${REPO_ROOT_DIR}"
 
-# Test ADB
-docker_run "adb shell getprop"
+# Download from Artifactory
+artifactory pull ihu_hourly_build-eng "${UPSTREAM_JOB_NUMBER}" out.tgz || die "artifactory pull failed"
 
-# Run tests
-docker_run "time python3 $REPO_ROOT_DIR"/vendor/volvocars/tools/ci/shipit/tester.py run --plan=gate -c ihu-generic adb mp-serial vip-serial
+# Unpack out.tgz
+tar xfz out.tgz || die "Unpack out.tgz failed"
+
+source "$REPO_ROOT_DIR"/build/envsetup.sh
+lunch ihu_vcc-eng
+
+# Update IHU
+ihu_update || die "Failed to flash IHU image"
+
+# Get properties
+adb shell getprop
+
+set +e
+# Run Unit and Component tests for vendor/volvocars
+#shellcheck disable=SC2086
+time python3 "$REPO_ROOT_DIR"/vendor/volvocars/tools/ci/shipit/tester.py run --plan=gate -c ihu-generic adb mp-serial vip-serial
+status=$?
+set -e
+
+# Check status
+if [ $status -ne 0 ]; then
+    die "Test failed"
+fi
