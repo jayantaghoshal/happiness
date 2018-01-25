4e32e271ed7ace1c21458845b77b6f7bc0342a37
Magnus Larsson <magnus.larsson.5@volvocars.com>
2018-01-25 12:30:19 +0100

    Include "Gate" test plan in "Hourly" plan
    Include the test_plan_gate in test_plan_hourly
    
    Change-Id: Ibe681743e73f4e8ccf71f72e16d1f4c5457b5fb9

--- test_plan.py
+++ test_plan.py
@@ -18,21 +18,20 @@ from shipit.test_runner.test_types import VTSTest, TradefedTest, Disabled, stand
 # These should mainly be used for system stability, ie when a failing module will
 # block other developers from continuing development. Such as "unit not booting", "adb not working", "black screen", etc
 test_plan_gate = [
-    VTSTest("vendor/volvocars/hardware/ci/test/ct/smoketest",                   standard_caps),
-    VTSTest("vendor/volvocars/hardware/signals/dataelements/test/ut",          standard_caps),
-    VTSTest("vendor/volvocars/hardware/tarmac/eventloop/test/ct",              standard_caps),
-    VTSTest("vendor/volvocars/hardware/localconfig/test/ct/localconfig_nativelib", standard_caps),
-    VTSTest("vendor/volvocars/hardware/localconfig/test/ut",                   standard_caps),
-    VTSTest("vendor/volvocars/hardware/netman/test/ct/ip_configuration",       standard_caps),
-    VTSTest("vendor/volvocars/hardware/netman/test/ct/iptables",               standard_caps),
-    VTSTest("vendor/volvocars/hardware/netman/test/ct/namespace_configuration",standard_caps),
-    VTSTest("vendor/volvocars/hardware/uds/test/ct",                           standard_caps),
-    VTSTest("vendor/volvocars/tools/test/daemon_test",                         standard_caps),
-    VTSTest("vendor/volvocars/tools/test/ui_performance/volvolauncher",         standard_caps),
+    VTSTest("vendor/volvocars/hardware/ci/test/ct/smoketest",                       standard_caps),
+    VTSTest("vendor/volvocars/hardware/signals/dataelements/test/ut",               standard_caps),
+    VTSTest("vendor/volvocars/hardware/tarmac/eventloop/test/ct",                   standard_caps),
+    VTSTest("vendor/volvocars/hardware/localconfig/test/ct/localconfig_nativelib",  standard_caps),
+    VTSTest("vendor/volvocars/hardware/localconfig/test/ut",                        standard_caps),
+    VTSTest("vendor/volvocars/hardware/netman/test/ct/ip_configuration",            standard_caps),
+    VTSTest("vendor/volvocars/hardware/netman/test/ct/iptables",                    standard_caps),
+    VTSTest("vendor/volvocars/hardware/netman/test/ct/namespace_configuration",     standard_caps),
+    VTSTest("vendor/volvocars/hardware/uds/test/ct",                                standard_caps),
+    VTSTest("vendor/volvocars/tools/test/daemon_test",                              standard_caps),
+    VTSTest("vendor/volvocars/tools/test/ui_performance/volvolauncher",             standard_caps),
 ]
 
-test_plan_hourly = [
-    VTSTest("vendor/volvocars/hardware/ci/test/ct/smoketest",                   standard_caps),
+test_plan_hourly = test_plan_gate + [
     Disabled(VTSTest("vendor/volvocars/hardware/climate/test/ct",                      standard_caps),
              reason="Viewclients vc.dump() doesn't work when the 'you are in fullscreen'-popup is stealing focus, HMI to fix",
              jira_issue="",
@@ -41,7 +40,7 @@ test_plan_hourly = [
     Disabled(VTSTest("vendor/volvocars/hardware/netman/test/ct/most_interface",       standard_caps),
              reason="MR1 not yet on master",
              jira_issue="",
-             deadline="2018-01-21"
+             deadline="2018-01-30"
     ),
     VTSTest("vendor/volvocars/hardware/dim/test/ct/apix",       standard_caps | {cp.apix}),
     # Climate included twice because the test case dynamically detects if FR hardware is present
@@ -49,12 +48,12 @@ test_plan_hourly = [
     Disabled(VTSTest("vendor/volvocars/hardware/ipcbd/test/ct",                  standard_caps),
              reason="Team slytherin working on the failing tests",
              jira_issue= "",
-             deadline= "2018-01-26"
+             deadline= "2018-01-30"
     ),
     Disabled(VTSTest("vendor/volvocars/hardware/test/iplm",                      standard_caps),
              reason="Team slytherin working on the failing tests",
              jira_issue = "",
-             deadline = "2018-01-26"
+             deadline = "2018-01-30"
     ),
     VTSTest("vendor/volvocars/hardware/test/gnss",                              standard_caps),
     VTSTest("vendor/volvocars/hardware/infotainmentIpBus/test/ut",              standard_caps),
@@ -75,7 +74,7 @@ test_plan_hourly = [
     Disabled(TradefedTest("vendor/volvocars/hardware/localconfig/test/ct/localconfig_java",       standard_caps),
              reason="Team slytherin working on the failing tests",
              jira_issue="",
-             deadline="2018-01-26"
+             deadline="2018-01-30"
              ),
     TradefedTest("vendor/volvocars/hardware/carconfig/carconfig_java_lib/test/ct/carconfig_api", standard_caps),
     TradefedTest("vendor/volvocars/hardware/netman/test/ct/InterfaceTest",       standard_caps),
@@ -101,4 +100,6 @@ test_plan_hourly = [
     )
 ]
 
-test_plan_nightly = test_plan_gate + test_plan_hourly
+test_plan_nightly = test_plan_hourly + [
+
+]
