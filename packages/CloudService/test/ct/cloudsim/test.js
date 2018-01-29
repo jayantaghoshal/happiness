ff2f6cf8277c3fe50c0f8edb1b320bd238884de5
Patrik Ingmarsson <patrik.2.ingmarsson@delphi.com>
2018-01-29 14:56:59 +0100

    Add cloud simulation to docker image
         - Add basic cloud simulation configuration for Cloud Service
    
    [PSS370-14136]
    
    Change-Id: I216bcdfc42b3467acc406cf932282526d6a532b4

---
+++ packages/CloudService/test/ct/cloudsim/test.js
@@ -0,0 +1,58 @@
+var jsonServer = require('/usr/local/lib/node_modules/json-server')
+var js2xmlparser = require('/usr/local/lib/node_modules/js2xmlparser')
+var server = jsonServer.create()
+var router = jsonServer.router('./db.json')
+var middlewares = jsonServer.defaults()
+
+const low = require('/usr/local/lib/node_modules/lowdb')
+const FileSync = require('/usr/local/lib/node_modules/lowdb/adapters/FileSync')
+
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+var routes = require('./routes.json')
+server.use(jsonServer.rewriter(routes))
+
+server.use(middlewares)
+server.use(jsonServer.bodyParser)
+
+router.render = function (req, res) {
+  var data = res.locals.data
+  var tag = ""
+  if(req._parsedUrl.pathname == '/entrypoint') {
+    tag = "entry_point"
+  } else if(req._parsedUrl.pathname == '/features-1') {
+    tag = "features"
+  } else if(req._parsedUrl.pathname == '/softwaremanagement-1') {
+    tag = "software_management"
+  } else if(req._parsedUrl.pathname == '/softwaremanagement-1/') {
+    tag = "software_management"
+  } else if(req._parsedUrl.pathname == '/availableUpdates') {
+    tag = "available_updates"
+  } else if(req._parsedUrl.pathname == '/commission') {
+    tag = "commission"
+  } else {
+    return res.send(data)
+  }
+  var str = js2xmlparser.parse(tag, data)
+  res.send(str)
+}
+
+server.post('/availableUpdates', function (req, res, next) {
+  return res.status(405).send({error: "Nope, cant do that."})
+})
+
+server.post('/commission', function (req, res, next) {
+  var tmp = db.get('availableUpdates').get('software').find({id: req.query.id})
+
+  db.get('downloads').get('software').push(tmp.value()).write()
+  db.get('availableUpdates').get('software').remove({id: req.query.id}).write()
+
+  req.method = 'GET'
+  next()
+})
+
+server.use(router)
+server.listen(3000, function () {
+  console.log('JSON Server is running')
+})
