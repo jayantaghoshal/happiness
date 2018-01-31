140fe938702e463021dbcf505907fd8376167a58
Parasto Pirshahid <parasto.pirshahid@delphi.com>
2018-01-31 15:23:53 +0100

    enabling mic test in hourly tests
    Change-Id: Idf3fa207e6deae499a061ef4e5a82b0dfb8cc45d

--- test_plan.py
+++ test_plan.py
@@ -78,11 +78,7 @@ test_plan_hourly = test_plan_gate + [
     TradefedTest("vendor/volvocars/tools/test/log_analyzer", standard_caps),
     VTSTest("vendor/volvocars/hardware/powermoding/test/ct", standard_caps | {cp.flexray}),
     VTSTest("vendor/volvocars/hardware/test/keypanel", standard_caps | {cp.flexray}),
-    Disabled(TradefedTest("vendor/volvocars/hardware/audio/test/ct/audio_mic_control/app", standard_caps),
-             reason="Waiting for microphone to be integrated",
-             jira_issue="",
-             deadline="2018-02-04"
-    ),
+    TradefedTest("vendor/volvocars/hardware/audio/test/ct/audio_mic_control/app", standard_caps | {cp.audio}),
     Disabled(TradefedTest("vendor/volvocars/hardware/audio/test/ct/audio_loopback/app",       standard_caps),
              reason="Waiting for fully working MOST drivers",
              jira_issue="",
