9da29dc90e3c2d1c3693ffe70d838fe7e4a5b9de
Krzysztof Wesolowski <krzysztof.wesolowski@volvocars.com>
2018-02-08 19:05:02 +0100

    Use hal_client_domain in sepolicy for netmand and uds_diagnostics
    [PSS370-13866]
    
    Change-Id: I1fa71c0e2478468bf4143cd66a4a7999522fd5a0

--- device/ihu_vcc/BoardConfig.mk
+++ device/ihu_vcc/BoardConfig.mk
@@ -6,9 +6,13 @@ include device/delphi/volvoihu/ihu_abl_car/BoardConfig.mk
 
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
-# TODO Enable once policies are updated
-BOARD_SEPOLICY_DIRS += \
-	vendor/volvocars/device/ihu_vcc/sepolicy
+# TODO Enable enforcing once policies are updated
+
+# content inspired by system/sepolicy/public - abstract hal / services declarations
+BOARD_SEPOLICY_DIRS += vendor/volvocars/device/ihu_vcc/sepolicy_hal_decl
+
+# concrete VCC implementation based on types provided in sepolicy_hal_decl
+BOARD_SEPOLICY_DIRS += vendor/volvocars/device/ihu_vcc/sepolicy
 
 TARGET_FS_CONFIG_GEN += vendor/volvocars/device/ihu_vcc/common/filesystem_config/config.fs
 
