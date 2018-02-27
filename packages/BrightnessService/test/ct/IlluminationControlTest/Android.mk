412634924e72719b9738ae517f9e2676bab7f79c
Tobias Ohrstrom <tobias.ohrstrom@volvocars.com>
2018-02-27 09:10:28 +0100

    Fix IlluminationControlTest to work correctly in CI
    * Changed out path of InjectFlexRayIlluminationTest component
    
    Change-Id: I1bdca4d6c9b693ee741f471be873fce4b421a869

--- packages/BrightnessService/test/ct/IlluminationControlTest/Android.mk
+++ packages/BrightnessService/test/ct/IlluminationControlTest/Android.mk
@@ -2,7 +2,6 @@
 # This file is covered by LICENSE file in the root of this project
 
 LOCAL_PATH := $(call my-dir)
-
 # Build java application for test
 
 include $(CLEAR_VARS)
@@ -21,25 +20,28 @@ LOCAL_PACKAGE_NAME := TestIlluminationControlJava
 LOCAL_COMPATIBILITY_SUITE := cts
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
+LOCAL_REQUIRED_MODULES := InjectFlexRayIllumination
 
 LOCAL_MULTILIB := 64
-include $(BUILD_CTS_PACKAGE)
 
-# Build cpp part of test
+include $(BUILD_CTS_PACKAGE)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := InjectFlexRayIllumination
-
-LOCAL_SRC_FILES := \
-    InjectFlexRayIlluminationTest/main.cpp \
+LOCAL_MODULE_TAGS := optional
+LOCAL_VENDOR_MODULE := true
+LOCAL_SRC_FILES := InjectFlexRayIlluminationTest/main.cpp
 
 LOCAL_SHARED_LIBRARIES += \
     liblog \
-    libtarmac \
     libdataelements \
 
+LOCAL_MULTILIB := 64
 LOCAL_CPPFLAGS += -fexceptions
 LOCAL_CPPFLAGS += -Wno-macro-redefined
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_DATA_APPS)
+ANDROID_PRODUCT_OUT:= $(TARGET_OUT_DATA_APPS)
 
 include $(BUILD_EXECUTABLE)
 
--- packages/BrightnessService/test/ct/IlluminationControlTest/prepare.sh
+++ packages/BrightnessService/test/ct/IlluminationControlTest/prepare.sh
@@ -4,4 +4,6 @@
 # This file is covered by LICENSE file in the root of this project
 
 adb remount
-adb push "${ANDROID_BUILD_TOP}/out/target/product/ihu_vcc/system/bin/InjectFlexRayIllumination" "/data/local/tmp/InjectFlexRayIllumination"
+adb shell log "before push InjectFlexRayIllumination"
+adb push "${ANDROID_BUILD_TOP}/out/target/product/ihu_vcc/data/app/InjectFlexRayIllumination" "/data/local/tmp/InjectFlexRayIllumination"
+adb shell log "after push"
