c6facdd8f388fee26829c14097633809d8979e2e
lveeraku <logesh.veera.kumar.kalyanasundaram@volvocars.com>
2018-02-28 12:43:23 +0100

    fix moving results to the web server
    Inside docker container, we will not have access to /cm/www dir so moved
    this code outside the docker.
    
    Change-Id: I48ae7c41bcc1c5b507a9d922f338bf4a01d2f68b

--- tools/ci/jenkins/jenkins_job_builder/jobs/bootstrap_ihu_hourly_daily_common.sh
+++ tools/ci/jenkins/jenkins_job_builder/jobs/bootstrap_ihu_hourly_daily_common.sh
@@ -28,3 +28,12 @@ cd "$WORKSPACE"
 
 # multiuser flag incase hourly and daily job uses docker at same time
 "$build_dir"/volvocars/tools/ci/jenkins/ci_docker_run.sh --multiuser "$build_dir"/volvocars/tools/ci/jenkins/report_generation.sh
+
+# deploy the result to the web server
+if echo "$JOB_NAME" | grep -i "daily"
+then
+    cp -R "$BUILD_NUMBER" /cm/www/daily_test_report/
+elif echo "$JOB_NAME" | grep -i "hourly"
+then
+    cp -R "$BUILD_NUMBER" /cm/www/hourly_test_report/
+fi
--- tools/ci/jenkins/report_generation.sh
+++ tools/ci/jenkins/report_generation.sh
@@ -41,13 +41,3 @@ python3 "$WORKSPACE"/vendor/volvocars/tools/ci/test_report_generation/test_repor
 
 python3 "$WORKSPACE"/vendor/volvocars/tools/ci/test_report_generation/load_results_to_mongodb.py
 
-cd "$WORKSPACE"
-
-# deploy it to the web server
-if echo "$JOB_NAME" | grep -i "daily"
-then
-    cp -R "$BUILD_NUMBER" /cm/www/daily_test_report/
-elif echo "$JOB_NAME" | grep -i "hourly"
-then
-    cp -R "$BUILD_NUMBER" /cm/www/hourly_test_report/
-fi
