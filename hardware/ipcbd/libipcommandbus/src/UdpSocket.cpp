c190dc04f12be22185b0df0fc5a5e8cc124abfdc
Shirolikar, Abhijeet <abhijeet.shirolikar@volvocars.com>
2018-03-05 09:11:05 +0000

    Revert "Fix to use broadcast port and Ip for Broadcast socket"
    This reverts commit a53a2a48e1ce84318ef7059023b74945fec0d778.
    
    This change should no longer be needed and can be reverted to get IPCBD simulator working
    
    Change-Id: I13ebe106f1e9e8802510f355afc6289c10f304ca

--- hardware/ipcbd/libipcommandbus/src/UdpSocket.cpp
+++ hardware/ipcbd/libipcommandbus/src/UdpSocket.cpp
@@ -60,9 +60,8 @@ void UdpSocket::setup(const Message::Ecu& ecu) {
                 return;
             }
 
-            // Use broadcast address and port for broadcast socket
+            // Use broadcast address for broadcast socket
             local_ip = it->second.ip;
-            local_port = it->second.port;
 
             set_option(SOL_SOCKET, SO_BROADCAST, true);
         }
