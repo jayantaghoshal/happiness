bb56dce5acfc60d1f32868d8e449a79a3f4b2c4b
Fredrik Henriksson <fredrik.henricsson@volvocars.com>
2018-03-13 15:54:07 +0100

    Add HMI commit check
    Add a commit check for HMI repos that support Gradle.
    The commit check is intended to provide quick feedback by
    building, running unit tests on host and doing static code analysis.
    If any of the quality gates fail, Jenkins should set a -1 Verified
    status.
    
    Tests and analysis are run through Gradle's check task, and it's
    up to each project to provide tests, which type of analysis
    should be run and how the output should be treated.
    
    [ARTINFO-2518]
    
    Change-Id: I5007a75065edaa27d76076d7cd4f1dc5328b64ee

---
+++ tools/ci/jenkins/commit_check_hmi.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Copyright 2018 Volvo Car Corporation
+# This file is covered by LICENSE file in the root of this project
+
+set -ex
+
+# Download the Android SDK Command Line Tools
+# Version reference: https://developer.android.com/studio/
+if [ -d "android-sdk-linux" ]; then
+  echo "SDK already downloaded."
+else
+  mkdir android-sdk-linux
+  (cd android-sdk-linux)
+  wget https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip
+  unzip sdk-tools-linux-3859397.zip
+  cd ..
+fi
+
+# Download the Android SDK
+export ANDROID_HOME="$PWD/android-sdk-linux"
+export PATH="$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$PATH"
+
+cd "$ANDROID_HOME"
+tools/bin/sdkmanager --update
+tools/bin/sdkmanager "platforms;android-27" "build-tools;27.0.3" "extras;google;m2repository" "extras;android;m2repository"
+yes | tools/bin/sdkmanager --licenses
+tools/bin/sdkmanager "platforms;android-P" "build-tools;28.0.0-rc1" "extras;google;m2repository" "extras;android;m2repository"
+yes | tools/bin/sdkmanager --licenses
+tools/bin/sdkmanager --update
+yes | tools/bin/sdkmanager --licenses
+
+cd "$WORKSPACE_ROOT"
+
+# Set the SDK dir for Gradle
+if ! grep -q "sdk.dir=$ANDROID_HOME" "$ZUUL_PROJECT/local.properties"; then
+  echo sdk.dir="$ANDROID_HOME" >> "$ZUUL_PROJECT/local.properties"
+fi
+
+# TODO: Run in sandbox and verify the output folder location and archive/result storage
+"$ZUUL_PROJECT"/gradlew clean assemble check -p "$ZUUL_PROJECT"
