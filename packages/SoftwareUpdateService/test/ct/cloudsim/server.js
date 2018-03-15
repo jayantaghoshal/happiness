c0bd12f2cbf7f2a764ca0df94ff022e479557a30
Martin Hansson <martin.hansson@volvocars.com>
2018-03-15 10:04:45 +0100

    Added ; for readability in js
    Change-Id: I737e635436c7987f082d9cdec8d92f78ed550410

--- packages/SoftwareUpdateService/test/ct/cloudsim/server.js
+++ packages/SoftwareUpdateService/test/ct/cloudsim/server.js
@@ -1,105 +1,105 @@
-var jsonServer = require('/usr/local/lib/node_modules/json-server')
-var js2xmlparser = require('/usr/local/lib/node_modules/js2xmlparser')
-var server = jsonServer.create()
-var router = jsonServer.router(__dirname + '/db.json')
-var middlewares = jsonServer.defaults({ static: __dirname + '/public' })
+var jsonServer = require('/usr/local/lib/node_modules/json-server');
+var js2xmlparser = require('/usr/local/lib/node_modules/js2xmlparser');
+var server = jsonServer.create();
+var router = jsonServer.router(__dirname + '/db.json');
+var middlewares = jsonServer.defaults({static:__dirname + '/public'});
 
-var db = router.db
+var db = router.db;
 
-var routes = require(__dirname + '/routes.json')
-server.use(jsonServer.rewriter(routes))
+var routes = require(__dirname + '/routes.json');
+server.use(jsonServer.rewriter(routes));
 
-server.use(middlewares)
-server.use(jsonServer.bodyParser)
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
 
 router.render = function (req, res) {
-  var data = res.locals.data
-  var tag = ""
+  var data = res.locals.data;
+  var tag = "";
   if (req._parsedUrl.pathname == '/entrypoint') {
-    tag = "entry_point"
+    tag = "entry_point";
   } else if (req._parsedUrl.pathname == '/features-1') {
-    tag = "features"
+    tag = "features";
   } else if (req._parsedUrl.pathname == '/softwaremanagement-1') {
-    tag = "software_management"
+    tag = "software_management";
   } else if (req._parsedUrl.pathname == '/softwaremanagement-1/') {
-    tag = "software_management"
+    tag = "software_management";
   } else if (req._parsedUrl.pathname == '/availableUpdates') {
-    tag = "available_updates"
+    tag = "available_updates";
   } else if (req._parsedUrl.pathname == '/commission') {
-    tag = "commission"
-  } else if (req._parsedUrl.pathname == '/pendingInstallations') {
-    tag = "pending_installations"
-  } else if (req._parsedUrl.pathname == '/downloads') {
-    tag = "downloads"
+    tag = "commission";
+  } else if(req._parsedUrl.pathname == '/pendingInstallations') {
+    tag = "pending_installations";
+  } else if(req._parsedUrl.pathname == '/downloads') {
+    tag = "downloads";
   }
-  else {
-    return res.send(data)
+   else {
+    return res.send(data);
   }
-  var str = js2xmlparser.parse(tag, data)
-  res.send(str)
+  var str = js2xmlparser.parse(tag, data);
+  res.send(str);
 }
 
 server.post('/availableUpdates', function (req, res, next) {
-  return res.status(405).send({ error: "Nope, cant do that." })
-})
+  return res.status(405).send({ error: "Nope, cant do that." });
+});
 
 
-var counter = -1
+var counter = -1;
 server.post('/commission', function (req, res, next) {
 
-  var t = db.get('availableUpdates').get('software')
+  var t = db.get('availableUpdates').get('software');
 
   for (i = 0; i < t.value().length; i++) {
     if (t.value()[i]['id'] == req.body.id) {
-      var tmp = t.value()[i]
+      var tmp = t.value()[i];
 
-      counter++
+      counter++;
 
-      var installation_order_data = {}
-      installation_order_data['id'] = counter
-      installation_order_data['status'] = "PENDING"
-      installation_order_data['created_by'] = "1FTKR1EDXBPB10452"
-      installation_order_data['created'] = "2002-05-30T09:00:00"
-      installation_order_data['downloads_uri'] = "/downloads/" //+ counter
-      installation_order_data['install_notifications_uri'] = "/installNotification"
-      installation_order_data['installation_report_uri'] = "/installationreport"
-      installation_order_data['software'] = [tmp]
-      var installation_order = {}
-      installation_order['installation_order'] = installation_order_data
+      var installation_order_data = {};
+      installation_order_data['id'] = counter;
+      installation_order_data['status'] = "PENDING";
+      installation_order_data['created_by'] = "1FTKR1EDXBPB10452";
+      installation_order_data['created'] = "2002-05-30T09:00:00";
+      installation_order_data['downloads_uri'] = "/downloads/";
+      installation_order_data['install_notifications_uri'] = "installnotifications";
+      installation_order_data['installation_report_uri'] = "/installationreport";
+      installation_order_data['software'] = [tmp];
+      var installation_order = {};
+      installation_order['installation_order'] = installation_order_data;
       db.get('pendingInstallations').get('installation_order').push(installation_order_data).write();
-      db.get('availableUpdates').get('software').remove(tmp).write()
+      db.get('availableUpdates').get('software').remove(tmp).write();
 
 
-      var d = db.get('preDownloadInfo')
+      var d = db.get('preDownloadInfo');
       for (j = 0; j < d.value().length; j++) {
         if (d.value()[j]['id'] == req.body.id) {
-          var tmpObj = Object.assign({}, d.value()[j])
-          tmpObj['id'] = req.body.id
-          tmpObj['installation_order_id'] = counter
-          db.get('downloads').push(tmpObj).write()
-          break
+          var tmpObj = Object.assign({}, d.value()[j]);
+          tmpObj['id'] = req.body.id;
+          tmpObj['installation_order_id'] = counter;
+          db.get('downloads').push(tmpObj).write();
+          break;
         }
       }
-      break
+      break;
     }
   }
 
-  req.method = 'GET'
-  next()
-})
+  req.method = 'GET';
+  next();
+});
 
 server.get('/downloads', function (req, res, next) {
-  var d = db.get('downloads')
+  var d = db.get('downloads');
   for (j = 0; j < d.value().length; j++) {
     if (d.value()[j]['installation_order_id'] == req.query.id) {
-      var tmpObj = d.value()[j]
-      var str = js2xmlparser.parse("downloads", tmpObj)
-      return res.send(str)
+      var tmpObj = d.value()[j];
+      var str = js2xmlparser.parse("downloads", tmpObj);
+      return res.send(str);
     }
   }
 
-  return res.status(405).send({ error: "Nope, cant do that." })
-})
+  return res.status(405).send({ error: "Nope, cant do that." });
+});
 
 function getValue(xml, tag) {
   var startTag = '<' + tag + '>';
@@ -138,9 +138,9 @@ server.post('/installNotification', function (req, res, next) {
 
   req.method = 'GET';
   next();
-})
+});
 
-server.use(router)
+server.use(router);
 server.listen(3000, function () {
-  console.log('JSON Server is running')
-})
+  console.log('JSON Server is running');
+});
