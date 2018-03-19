d81f3309ac1847247f93336d651e8b692212b3fc
Bjorn Furugard <bjorn.furugard@volvocars.com>
2018-03-19 13:26:18 +0100

    Add argument to control length of power cycle
    Change-Id: I67da272418d6f6880488cefa2a3c24937a200d2f

--- tools/ci/jenkins/ihu_ipm_reboot.py
+++ tools/ci/jenkins/ihu_ipm_reboot.py
@@ -7,8 +7,14 @@ import os
 import json
 # IP Power Management
 from remote_power_control.IHU_power_control import IHU_power_control
+import sys
 
 def main():
+    power_cycle_length = 120
+    try:
+        power_cycle_length = int(sys.argv[1])
+    except Exception:
+        pass
     try:
         mapping_file = os.environ['IPM_MAP_FILE']  # Set in jenkins build env
         json.load(open(mapping_file))
@@ -16,7 +22,7 @@ def main():
         raise RuntimeError('Failed due to the IPM map file')
     ipm_instance = IHU_power_control()
     ipm_instance.port_detail()
-    ipm_instance.reboot(120) # reboot after 2 minutes
+    ipm_instance.reboot(power_cycle_length) # Turns off power, waits <power_cycle_length> and powers on again
     print("State: "+str(ipm_instance.state()))
 
 if __name__ == '__main__':
