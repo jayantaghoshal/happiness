1b7a9375caafbf9f024220e2ac5f44da69084587
Erik Elmeke <erik.elmeke@volvocars.com>
2018-03-23 13:29:07 +0100

    Android mk linter
    Change-Id: I0d8c6f8de5b8edad4a4b8e0c8d5c7a9f53266df0

---
+++ tools/lib/python/android_mk_linter.py
@@ -0,0 +1,18 @@
+import re
+
+
+def is_assigned_variable_invalid(name):
+    return (name.startswith("PRODUCT_") or
+            name.startswith("ANDROID_"))
+
+def lint_android_mk_file(filename):
+    if "vendor/volvocars/device" in filename:
+        return
+
+    with open(filename, "r", encoding="utf-8") as f:
+        file_contents = f.read()
+
+    for match in re.finditer(r"([\w\{\}\$]+)\s*:=", file_contents):
+        assigned_variable_name = match.group(1).strip()
+        if is_assigned_variable_invalid(assigned_variable_name):
+            yield "Error: %s. Not allowed to assign to variable %s" % (filename, assigned_variable_name)
