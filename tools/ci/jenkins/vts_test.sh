163a5a336e9a4aa3f59f43ca79f852e5e9356d82
Gustav Svensson <gustav.svensson@volvocars.com>
2018-04-04 15:57:06 +0200

    Storing VTS results in MongoDB
    Results for each module in the VTS are parsed from the file test_result.xml
    and uploaded to MongoDB to there be displayed among other results.
    
    Change-Id: I76c23da032f5ec6d5f7b26b88cca67d5a18f7102
    Jira: ARTINFO-2188

--- tools/ci/jenkins/vts_test.sh
+++ tools/ci/jenkins/vts_test.sh
@@ -58,6 +58,9 @@ set +e
 #TODO Copy this to the above if statement as the command for cts differ a bit?
 time vts-tradefed run commandAndExit "$REPO_ROOT_DIR"/vendor/volvocars/tools/ci/test/"${plan}" --skip-all-system-status-check --skip-preconditions #--abi x86_64
 
+# Upload results to MongoDB
+python3 "$REPO_ROOT_DIR"/vendor/volvocars/tools/ci/shipit/handle_result/store_vts_results.py ./out/host/linux-x86/vts/android-vts/results/
+
 # Compare to last run here and fail if new errors are found
 mkdir old
 (cd old; artifactory pull-latest "${JOB_NAME}" || echo "artifactory pull failed")
