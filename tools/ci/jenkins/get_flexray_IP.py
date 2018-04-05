d40b5edfe5d9ce60e49e04407580fbb763ae33be
Gustav Svensson <gustav.svensson@volvocars.com>
2018-04-05 10:50:13 +0200

    Print error in case json parsing fails
    When the json file fails to be parsed a long stacktrace is printed
    This aims to only print the necessary error message which should
    be easier to debug.
    
    Change-Id: Iec92fc89ce3386d9df01c02fd769985d95a62531

--- tools/ci/jenkins/get_flexray_IP.py
+++ tools/ci/jenkins/get_flexray_IP.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 
-# Copyright 2017 Volvo Car Corporation
+# Copyright 2018 Volvo Car Corporation
 # This file is covered by LICENSE file in the root of this project
 
 import os
 import json
+import sys
 
 def main():
     mapping_file = os.environ['IPM_MAP_FILE']  # Set in jenkins build env
-    lab_config_data=json.load(open(mapping_file))
+    try:
+        lab_config_data=json.load(open(mapping_file))
+    except json.JSONDecodeError as e:
+        print("Error while parsing IPM_MAP_FILE: {}".format(mapping_file))
+        print("Parsing error: {}".format(e))
+        sys.exit(1)
     hostname = os.environ["HOST_HOSTNAME"]
     flexray_name = lab_config_data[hostname]["flexray"]
     print(lab_config_data[flexray_name]["ip"])
