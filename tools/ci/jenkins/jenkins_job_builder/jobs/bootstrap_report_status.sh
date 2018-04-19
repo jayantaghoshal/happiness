eb73a19476d4d3c1be5f410d2b68cf5603467e43
Gustav Svensson <gustav.svensson@volvocars.com>
2018-04-19 09:17:46 +0200

    Inluding and running emails in docker
    In order to have python3 and to run repo we need to first download a docker.
    
    Change-Id: I5814545ce52eefdf3457a22be56747f5475ec44e

--- tools/ci/jenkins/jenkins_job_builder/jobs/bootstrap_report_status.sh
+++ tools/ci/jenkins/jenkins_job_builder/jobs/bootstrap_report_status.sh
@@ -3,6 +3,8 @@
 # Copyright 2018 Volvo Car Corporation
 # This file is covered by LICENSE file in the root of this project
 
+set +e
+
 echo "Reporting job status"
 
 # This file needs to be created last in the job.
@@ -14,6 +16,7 @@ else
     BUILD_STATUS=fail
 fi
 
-bootstrap_docker_run "repo init -u ${GIT_URL} -b ${GIT_COMMIT}" || die "repo init failed"
-bootstrap_docker_run "repo sync --no-clone-bundle --current-branch -q -j4 vendor/volvocars" || die "repo sync failed"
-python3 vendor/volvocars/tools/ci/shipit/report_job_status.py "${JOB_NAME}" "${BUILD_NUMBER}" "${BUILD_STATUS}" "${GIT_COMMIT}"
+bootstrap_docker_run "repo init -u ${GIT_URL} -b ${GIT_COMMIT}" || echo "repo init failed" && exit 0
+bootstrap_docker_run "repo sync --no-clone-bundle --current-branch -q -j4 vendor/volvocars" || echo "repo sync failed" && exit 0
+
+./vendor/volvocars/tools/ci/jenkins/ci_docker_run.sh python3 vendor/volvocars/tools/ci/shipit/report_job_status.py "${JOB_NAME}" "${BUILD_NUMBER}" "${BUILD_STATUS}" "${GIT_COMMIT}" || echo "report job status failed" && exit 0
