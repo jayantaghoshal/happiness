6fca32bcdb53ece556467be5d9d20211904f3b73
Gustav Svensson <gustav.svensson@volvocars.com>
2018-05-02 09:09:35 +0200

    Retry repo sync before test.
    Du to local changes this job failed:
    https://icup_android.jenkins.cm.volvocars.biz/job/ihu_hourly_test-audio/469/console
    With a hard clean and a retry it would have worked.
    
    Change-Id: I3d2b1af8a6925f58ba5cf50680cef9b0df3688b8

--- tools/ci/jenkins/jenkins_job_builder/jobs/bootstrap_ihu_repo_sync_before_test.sh
+++ tools/ci/jenkins/jenkins_job_builder/jobs/bootstrap_ihu_repo_sync_before_test.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
-# Copyright 2017 Volvo Car Corporation
+# Copyright 2017-2018 Volvo Car Corporation
 # This file is covered by LICENSE file in the root of this project
 
 ##
 # Repo sync
 #
-bootstrap_docker_run "repo init -u ${UPSTREAM_JOB_GIT_URL} -b ${UPSTREAM_JOB_GIT_REVISION}" || die "repo init failed"
-#fetching only the required codes for running the tests
-SYNC_OPTIONS="--no-clone-bundle --force-sync --detach --optimized-fetch --current-branch -q -j4"
-SYNC_REPOSITORIES="aosp/platform/build/soong \
-aosp/platform/build/blueprint \
-aosp/platform/prebuilts/go/linux-x86 \
-aosp/platform/prebuilts/build-tools \
-vendor/volvocars \
-aosp/platform/build \
-bsp/device/delphi/volvoihu \
-aosp/platform/packages/services/Car \
-aosp/device/sample \
-vendor/delphi/android_devices \
-vendor/delphi/bb_reprogramming \
-vendor/delphi/binaries_android_tunermanager \
-vendor/delphi/android_diagnostics \
-vendor/delphi/binaries_android_traffic \
-vendor/google/apps/GAS"
-bootstrap_docker_run "repo sync ${SYNC_OPTIONS} ${SYNC_REPOSITORIES}" || die "repo sync failed"
+function get_repos {
+    bootstrap_docker_run "repo init -u ${UPSTREAM_JOB_GIT_URL} -b ${UPSTREAM_JOB_GIT_REVISION}" || die "repo init failed"
+    #fetching only the required codes for running the tests
+    SYNC_OPTIONS="--no-clone-bundle --force-sync --detach --optimized-fetch --current-branch -q -j4"
+    SYNC_REPOSITORIES="aosp/platform/build/soong \
+    aosp/platform/build/blueprint \
+    aosp/platform/prebuilts/go/linux-x86 \
+    aosp/platform/prebuilts/build-tools \
+    vendor/volvocars \
+    aosp/platform/build \
+    bsp/device/delphi/volvoihu \
+    aosp/platform/packages/services/Car \
+    aosp/device/sample \
+    vendor/delphi/android_devices \
+    vendor/delphi/bb_reprogramming \
+    vendor/delphi/binaries_android_tunermanager \
+    vendor/delphi/android_diagnostics \
+    vendor/delphi/binaries_android_traffic \
+    vendor/google/apps/GAS"
+    bootstrap_docker_run "repo sync ${SYNC_OPTIONS} ${SYNC_REPOSITORIES}" || die "repo sync failed"
+}
+function clean_all {
+    find . -type f -delete && find . -type d -empty -delete
+}
+get_repos || (clean_all && get_repos)
