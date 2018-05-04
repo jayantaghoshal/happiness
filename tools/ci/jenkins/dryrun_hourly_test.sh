b47e099a9de28201f17718d5a92558548819d2c3
Per Karlsson <per.karlsson.2@volvocars.com>
2018-05-04 17:40:07 +0200

    CI: Add testscript for dry-run with hourly testplan
    Change-Id: I30772e1f4be95ebca9a252e8f5588d6daf69de3a

---
+++ tools/ci/jenkins/dryrun_hourly_test.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Copyright 2018 Volvo Car Corporation
+# This file is covered by LICENSE file in the root of this project
+
+set -ex
+SCRIPT_DIR=$(cd "$(dirname "$(readlink -f "$0")")"; pwd)
+source "${SCRIPT_DIR}/common.sh"
+REPO_ROOT_DIR=$(readlink -f "${SCRIPT_DIR}"/../../../../..)
+
+# Update the manifests based on the templates for gate test
+time python3 ./vendor/volvocars/tools/ci/shipit/bump.py . local no_sync "${ZUUL_PROJECT}"
+# Sync repos required for build/envsetup.sh and lunch so we can run VTS.
+repo_sync aosp/platform/build/soong aosp/platform/build/blueprint aosp/platform/prebuilts/go/linux-x86 aosp/platform/prebuilts/build-tools
+repo_sync aosp/platform/build bsp/device/delphi/volvoihu aosp/platform/packages/services/Car aosp/device/sample
+repo_sync vendor/delphi/android_devices vendor/delphi/bb_reprogramming vendor/delphi/binaries_android_tunermanager vendor/delphi/android_diagnostics vendor/delphi/binaries_android_traffic
+# repo sync vendor/google/apps/GAS only needed since gms.mk is included by other makefiles called by lunch
+repo_sync vendor/google/apps/GAS
+
+source "$REPO_ROOT_DIR"/build/envsetup.sh
+lunch ihu_vcc-eng
+source "$REPO_ROOT_DIR"/vendor/volvocars/tools/envsetup.sh
+
+# Pull out files uploaded to Artifactory in gate_build.sh.
+OUT_ARCHIVE=out.tgz
+rm -rf out "${OUT_ARCHIVE}"
+
+artifactory pull ihu_gate_build "${ZUUL_COMMIT}" "${OUT_ARCHIVE}" \
+    || die "Could not pull out archive from Artifactory."
+
+tar xvf "${OUT_ARCHIVE}" || die "Could not extract out archive."
+rm "${OUT_ARCHIVE}"
+
+capability=""
+if echo "${JOB_NAME}" | grep "test-flexray" >/dev/null; then
+    capability="flexray"
+    export VECTOR_FDX_IP
+    VECTOR_FDX_IP=$(python3 "$REPO_ROOT_DIR"/vendor/volvocars/tools/ci/jenkins/get_flexray_IP.py)
+    ping -c1 "${VECTOR_FDX_IP}"
+elif echo "${JOB_NAME}" | grep "test-audio" >/dev/null; then
+    capability="audio"
+elif echo "${JOB_NAME}" | grep "test-cem" >/dev/null; then
+    capability="cem"
+elif echo "${JOB_NAME}" | grep "test-apix" >/dev/null; then
+    capability="apix"
+fi
+export capability
+
+set +e
+
+# Run Unit and Component tests for vendor/volvocars
+#shellcheck disable=SC2086
+time python3 "$REPO_ROOT_DIR"/vendor/volvocars/tools/ci/shipit/tester.py run --plan hourly --report_results_to_ci_database --update_ihu -c ihu-generic adb mp-serial vip-serial ${capability} -o ${capability}
+status=$?
+
+set -e
+
+echo "Logs and results are stored in db"
+
+# Check status
+if [ $status -ne 0 ]; then
+    die "Test failed"
+fi
+
