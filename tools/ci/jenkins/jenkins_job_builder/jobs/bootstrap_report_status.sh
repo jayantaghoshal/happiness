b22df88c7441f702448199609a8f0aeaaa78d35e
Gustav Svensson <gustav.svensson@volvocars.com>
2018-05-07 16:06:00 +0200

    Use jenkins api for job status report
    Change-Id: I03ab05bc0c116b2e7cfa90f49a9498ac79e5dab3

--- tools/ci/jenkins/jenkins_job_builder/jobs/bootstrap_report_status.sh
+++ tools/ci/jenkins/jenkins_job_builder/jobs/bootstrap_report_status.sh
@@ -7,21 +7,15 @@ set +e
 
 echo "Reporting job status"
 
-# This file needs to be created last in the job.
-# That only happens if the job has been successful so far.
-if [ -f "${JOB_NAME}".success ];
-then
-    BUILD_STATUS=pass
-else
-    BUILD_STATUS=fail
-fi
 get_repos() {
     (bootstrap_docker_run "repo init --reference=/cm/repo-mirror -u ${GIT_URL} -b ${GIT_COMMIT}" || (echo "repo init failed" && exit 1)) &&
-    (bootstrap_docker_run "repo sync --no-clone-bundle --current-branch -q -j4 vendor/volvocars" || (echo "repo sync failed" && exit 1))
+    (bootstrap_docker_run "repo sync --no-clone-bundle --current-branch --force-sync -q -j4 vendor/volvocars" || (echo "repo sync failed" && exit 1))
 }
+
 clean_all() {
     find . -type f -delete && find . -type d -empty -delete
 }
+
 (get_repos || (clean_all && get_repos)) &&
-(./vendor/volvocars/tools/ci/jenkins/ci_docker_run.sh python3 vendor/volvocars/tools/ci/shipit/report_job_status.py "${JOB_NAME}" "${BUILD_NUMBER}" "${BUILD_STATUS}" "${GIT_COMMIT}" || (echo "report job status failed" && exit 1)) ||
+(./vendor/volvocars/tools/ci/jenkins/ci_docker_run.sh python3 vendor/volvocars/tools/ci/shipit/report_job_status.py "${JOB_NAME}" "${BUILD_NUMBER}" "${GIT_COMMIT}" || (echo "report job status failed" && exit 1)) ||
 exit 0 # always green
--- tools/ci/shipit/report_job_status.py
+++ tools/ci/shipit/report_job_status.py
@@ -6,6 +6,7 @@ from shipit import reporter
 import os
 import json
 import logging.config
+from jenkins import Jenkins
 
 # Used to report hourly/daily test status from Jenkins
 
@@ -19,6 +20,18 @@ po_emails = {
 }
 
 
+def get_job_status(jenkins_job_name: str, jenkins_build_number: str):
+    jenkins = Jenkins('https://icup_android.jenkins.cm.volvocars.biz',
+            username=os.environ['JENKINS_USER'],
+            password=os.environ['JENKINS_API_PASSWORD'])
+    build_info = jenkins.get_build_info(jenkins_job_name, int(jenkins_build_number), depth=0)
+    status = 'pass'
+    for build in build_info['subBuilds']:
+        if not build['result'] == 'SUCCESS':
+            status = 'fail'
+    return status
+
+
 def main():
     with open(os.path.join(os.path.dirname(__file__), "logging.json"), "rt") as f:
         log_config = json.load(f)
@@ -26,13 +39,14 @@ def main():
 
     jenkins_job_name = sys.argv[1]
     jenkins_build_number = sys.argv[2]
-    status = sys.argv[3].lower() #fail/pass
-    git_hash = sys.argv[4]
+    git_hash = sys.argv[3]
 
     try:
         int(jenkins_build_number)
     except ValueError as e:
         raise Exception('Unknown value for jenkins build number')
+
+    status = get_job_status(jenkins_job_name, jenkins_build_number)
 
     if jenkins_job_name == 'ihu_hourly':
         if status == 'pass':
