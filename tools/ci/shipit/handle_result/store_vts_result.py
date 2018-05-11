95f6d665915b606cd6fec1c277439ec91e5055ce
HUI <hui.zhou@volvocars.com>
2018-05-11 11:02:24 +0000

    Include VTS test case status to overview dashboard
    ARTINFO-3448
    
    Change-Id: I7d9dc376258ed02a604ee843a06f86e820344ad3

--- tools/ci/shipit/handle_result/store_vts_result.py
+++ tools/ci/shipit/handle_result/store_vts_result.py
@@ -8,6 +8,9 @@ import xml.etree.ElementTree as ET
 import json
 import mongodb_wrapper
 from typing import Any, Dict
+from .influxdb_wrapper import insert_influx_data
+import logging
+logger = logging.getLogger(__name__)
 
 def extract_files(path):
     found = False
@@ -21,13 +24,13 @@ def extract_files(path):
 
 def unzip(folder, file):
     try:
-        print("Unpacking file: {}".format(os.path.join(folder, file)))
+        logger.debug("Unpacking file: {}".format(os.path.join(folder, file)))
         zip_ref = zipfile.ZipFile(os.path.join(folder, file), 'r')
         zip_ref.extractall(folder)
         zip_ref.close()
         return True
     except Exception:
-        print("Could not unpack file: " + str(os.path.join(folder, file)))
+        logger.warning("Could not unpack file: " + str(os.path.join(folder, file)))
         return False
 
 
@@ -45,7 +48,7 @@ def upload_results(file):
     root = tree.getroot()
 
     if root.tag == 'Result':
-        print('Found results')
+        logger.info('Found results')
     else:
         sys.exit(1)
 
@@ -59,8 +62,11 @@ def upload_results(file):
     json_base["top_test_job_build_number"] = int(os.environ["TOP_JOB_NUMBER"])
     json_base["top_test_job_name"] = "ihu_vts"
     json_base["test_type"] = 'AndroidVTS'
-
+    totalmodulecount = 0
+    totaltestcasecount = 0
+    failingtestcasecount = 0
     for module in root.findall('Module'):
+        totalmodulecount += 1
         module_json = {**json_base, **module.attrib}
         module_json['test_dir_name'] = module.attrib['name']
         module_json['module_name'] = module.attrib['name']
@@ -69,28 +75,51 @@ def upload_results(file):
         module_json['testcases'] = [{'tests': [], 'testcase_name': testcase.attrib['name']}]
         result = True
         for test in testcase.findall('Test'):
+            totaltestcasecount += 1
             module_json['testcases'][0]['tests'].append({'name': test.attrib['name'], 'status': test.attrib['result']})
             if not test.attrib['result'] == 'pass':
                 result = False
+                failingtestcasecount += 1
         module_json["result"] = result
 
-        print(module_json)
+        logger.info("Inserting VTS test data: {}".format(module_json))
         mongodb_wrapper.insert_data(module_json)
+
+    data = {
+        'build_number' : os.environ["BUILD_NUMBER"],
+        'build_host' : os.environ["HOST_HOSTNAME"],
+        'job_name' : os.environ["JOB_NAME"],
+        'top_test_job_build_number' : os.environ["TOP_JOB_NUMBER"],
+        'top_test_job_name' : "ihu_vts",
+        'total_test_modules' : totalmodulecount,
+        'total_testcases' : totaltestcasecount,
+        'passing_testcases' : totaltestcasecount - failingtestcasecount,
+        'failing_testcases' : failingtestcasecount,
+    }
+    tags = {
+        "jobType": "AndroidVTS",
+    }
+
+    logger.info("Storing VTS test status: {}".format(data))
+    try:
+        insert_influx_data(str(os.environ["JOB_NAME"]), data, tags)
+    except Exception as e:
+        logger.info(str(e))
 
 
 def main():
     if len(sys.argv) < 2:
-        print("Need one folder as argument, exit")
+        logger.error("Need one folder as argument, exit")
         sys.exit(1)
     folder = sys.argv[1]
 
     if not extract_files(folder):
-        print("No zip files found in folder ({}), aborting upload".format(folder))
+        logger.error("No zip files found in folder ({}), aborting upload".format(folder))
         sys.exit(1)
 
     file = get_result_file(folder)
     if file is None:
-        print("No results found in folder ({}), aborting upload".format(folder))
+        logger.error("No results found in folder ({}), aborting upload".format(folder))
         sys.exit(1)
 
     upload_results(get_result_file(folder))
