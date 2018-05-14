7f4c7dbb82db32ba56557188b50a823871175df3
Mikael Olsson <mikael.olsson.2@volvocars.com>
2018-05-14 13:17:42 +0200

    Add EVS hardware service registration retry loop
    Because the EVS hardware service is started so early in the boot
    sequence, the Android service manager may not be ready when the EVS
    hardware service tries to register itself. This will cause the
    registration to fail and the EVS hardware service to stop.
    
    To resolve this, a retry loop has been added to allow the EVS hardware
    service to handle this situation gracefully and wait for the Android
    service manager to get ready.
    
    Tracked-On: ARTINFO-3459
    Change-Id: I7d02ae70031c3c7d81357ee0f5110eed9acba345

--- hardware/pac/evs/hardware/src/evs_hardware.cpp
+++ hardware/pac/evs/hardware/src/evs_hardware.cpp
@@ -3,15 +3,18 @@
  * This file is covered by LICENSE file in the root of this project
  */
 
+#include <chrono>
+#include <cinttypes>
+#include <cstdint>
 #include <cstdlib>
+#include <thread>
 
-#include <cutils/properties.h>
 #include <hidl/HidlTransportSupport.h>
 #include <utils/StrongPointer.h>
 
+#include <evs/hardware.h>
 #include <libdbg.h>
 #include <pacconfig/config.h>
-#include "evs/hardware.h"
 #include "evs_enumerator.h"
 
 // libhidl
@@ -28,40 +31,51 @@ using android::status_t;
 // libpacconfig
 using ::pac::config::PacConfig;
 
+// thread
+using std::this_thread::sleep_for;
+
 // VCC implementation
 using android::hardware::automotive::evs::V1_0::vcc_implementation::EvsEnumerator;
 
 namespace {
 namespace service_cfg {
 constexpr int kMaxNumberOfThreads = 1;
+
 // Note that maxThreads must include the caller thread if callerWillJoin is true
 constexpr bool kCallerWillJoin = true;
+
+// How many milliseconds the service shall wait before retrying to register its enumerator
+constexpr uint32_t kRegisterRetryInterval = 100;
 }  // namespace service_cfg
 }  // namespace
 
 int main() {
     dbgD("EVS hardware service is starting...");
-    android::sp<IEvsEnumerator> service;
-    status_t status;
 
     if (!PacConfig::Evs::IsEvsServicesEnabled()) {
         dbgD("EVS hardware service did not start since it is disabled!");
         return EXIT_SUCCESS;
     }
 
-    service = new EvsEnumerator();
     configureRpcThreadpool(service_cfg::kMaxNumberOfThreads, service_cfg::kCallerWillJoin);
-    status = service->registerAsService(kEvsHardwareServiceName);
-    if (status == OK) {
-        dbgD("EVS hardware service started. %s is ready!", kEvsHardwareServiceName);
 
-        joinRpcThreadpool();
+    android::sp<IEvsEnumerator> service = new EvsEnumerator();
+    status_t status;
+    do {
+        status = service->registerAsService(kEvsHardwareServiceName);
+        if (status != OK) {
+            dbgW("Failed to register service %s (%d), will retry in %" PRIu32 "ms",
+                 kEvsHardwareServiceName,
+                 status,
+                 service_cfg::kRegisterRetryInterval);
+            sleep_for(std::chrono::milliseconds(service_cfg::kRegisterRetryInterval));
+        }
+    } while (status != OK);
 
-        // In normal operation, we don't expect the thread pool to exit
-        dbgE("EVS hardware service was interrupted, shutting down!");
-    } else {
-        dbgE("Could not register service %s (%d).", kEvsHardwareServiceName, status);
-    }
+    dbgD("EVS hardware service started. %s is ready!", kEvsHardwareServiceName);
+    joinRpcThreadpool();
 
+    // In normal operation, we don't expect the thread pool to exit
+    dbgE("EVS hardware service was interrupted, shutting down!");
     return EXIT_FAILURE;
 }
