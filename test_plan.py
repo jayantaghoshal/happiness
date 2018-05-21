aa739637e7666ac42238fa2e319b8776a347d614
Samuel Idowu <samuel.idowu@volvocars.com>
2018-05-21 05:34:18 +0000

    Run test for Convinence API's RTP netns bridging on a separate CI rig.
     - Convinence API is disabled by default on master, and related tests
       will run on a separate rig in CI with special config.
    
    Change-Id: I0506955fc5bede4d6a01a2af4299c818d4e6068a

--- test_plan.py
+++ test_plan.py
@@ -108,7 +108,7 @@ test_plan_staging_daily = [
     VTSTest("vendor/volvocars/hardware/vehicle/test/Vhal_SmokeTest",                standard_caps), #Praveen Kumar Khatri
     TradefedTest("vendor/volvocars/hardware/netman/test/ct/usb2ethernet",           standard_caps), #Uguudei
     VTSTest("vendor/volvocars/hardware/vehicle/test/speed_limit_adaptation", standard_caps | {cp.flexray}), #Justin Saler
-    VTSTest("vendor/volvocars/hardware/netman/test/ct/rtp_audio_namespace_bridge", standard_caps), #Samuel Idowu
+    VTSTest("vendor/volvocars/hardware/netman/test/ct/rtp_audio_namespace_bridge", standard_caps  | {cp.audio}), #Samuel Idowu
     VTSTest("vendor/volvocars/hardware/remotectrl/test/it/climatectrl_tests", standard_caps | {cp.flexray} | {cp.audio}), #Abhijeet Shirolikar
     VTSTest("vendor/volvocars/hardware/vehicle/test/climate_firstrow/ct/temperature_adjustment", standard_caps | {cp.flexray}), #Justin Saler
     VTSTest("vendor/volvocars/hardware/vehicle/test/climate_firstrow/ct/air_condition", standard_caps | {cp.flexray_or_carsim}), #Kirti Goel
