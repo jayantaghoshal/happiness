0ac6f6fbfd6071cec6b38a7d250cca468e6dcd44
HUI <hui.zhou@volvocars.com>
2018-05-23 10:50:44 +0000

    value tag mismatching in kpi dash
    NO-JIRA
    
    Change-Id: Ia1137aad5e52b474ddbdd22e520b1fc1799bc99e

--- hardware/ci/test/ct/smoketest/VtsCiSmokeTest.py
+++ hardware/ci/test/ct/smoketest/VtsCiSmokeTest.py
@@ -159,8 +159,10 @@ class VtsCiSmokeTest(ihu_base_test.IhuBaseTestClass):
             disk_usage_output = str(disk_load[str(disk)]['usage'][3])
             if disk_usage_output[-1] == "%":
                 disk_usage_output = disk_usage_output[:-1]
+            disk_usage_name = str(disk_load[str(disk)]['usage'][4])
+            disk_usage_name = disk_usage_name.replace('/', '_')
 
-            self.write_kpi("disk_usage" + str(disk_load[str(disk)]['usage'][4]), float(disk_usage_output), "%")
+            self.write_kpi("disk_usage" + disk_usage_name, float(disk_usage_output), "%")
 
             if (float(disk_load[str(disk)]['usage'][3].strip('%'))) > requirement:
                asserts.assertLess(float(disk_load[str(disk)]['usage'][3].strip('%')), requirement, "The disk usage is over " + str(requirement) + "%")
