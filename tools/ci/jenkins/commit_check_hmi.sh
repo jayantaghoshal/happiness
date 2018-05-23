f226b053a5fa2ffbb7e862d1a31f50a50858cad9
Kenneth Karlsson <kenneth.karlsson.2@volvocars.com>
2018-05-23 13:55:35 +0200

    Fix the path for the sdkmanager
    Again...
    
    No-Jira
    
    Change-Id: I411beed81ad5f004122dee3000051dd1c68394ca

--- tools/ci/jenkins/commit_check_hmi.sh
+++ tools/ci/jenkins/commit_check_hmi.sh
@@ -16,15 +16,15 @@ fi
 
 # Download the Android SDK
 export ANDROID_HOME="$PWD/android-sdk-linux"
-export PATH="$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$PATH"
+export PATH="$ANDROID_HOME"/tools:$ANDROID_HOME/platform-tools:$PATH
 
-"$ANDROID_HOME/tools/bin/sdkmanager --update"
-"$ANDROID_HOME/tools/bin/sdkmanager "platforms;android-27" "build-tools;27.0.3" "extras;google;m2repository" "extras;android;m2repository""
-"yes | $ANDROID_HOME/tools/bin/sdkmanager --licenses"
-"$ANDROID_HOME/tools/bin/sdkmanager "platforms;android-P" "build-tools;28.0.0-rc1" "extras;google;m2repository" "extras;android;m2repository""
-"yes | $ANDROID_HOME/tools/bin/sdkmanager --licenses"
-"$ANDROID_HOME/tools/bin/sdkmanager --update"
-"yes | $ANDROID_HOME/tools/bin/sdkmanager --licenses"
+"$ANDROID_HOME"/tools/bin/sdkmanager --update
+"$ANDROID_HOME"/tools/bin/sdkmanager "platforms;android-27" "build-tools;27.0.3" "extras;google;m2repository" "extras;android;m2repository"
+yes | "$ANDROID_HOME"/tools/bin/sdkmanager --licenses
+"$ANDROID_HOME"/tools/bin/sdkmanager "platforms;android-P" "build-tools;28.0.0-rc1" "extras;google;m2repository" "extras;android;m2repository"
+yes | "$ANDROID_HOME"/tools/bin/sdkmanager --licenses
+"$ANDROID_HOME"/tools/bin/sdkmanager --update
+yes | "$ANDROID_HOME"/tools/bin/sdkmanager --licenses
 
 # Set the SDK dir for Gradle
 if ! grep -q "sdk.dir=$ANDROID_HOME" "$ZUUL_PROJECT/local.properties"; then
