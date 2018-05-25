1626ac2f486a48cd0696ad303ef1b07e696f491d
Kenneth Karlsson <kenneth.karlsson.2@volvocars.com>
2018-05-25 14:22:18 +0200

    Correction to the progregression gate job
    No-Jira
    
    Change-Id: I90abf43ecca6df3b57ee1d353e0c334505346853

--- tools/ci/jenkins/sdk_host_library.sh
+++ tools/ci/jenkins/sdk_host_library.sh
@@ -4,4 +4,4 @@
 # This file is covered by LICENSE file in the root of this project
 
 # TODO, build and do host tests
-"$ZUUL_PROEJCT"/gradlew artifactoryPublish
+"$ZUUL_PROJECT"/gradlew artifactoryPublish
