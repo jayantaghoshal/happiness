3536fe6c3fd6f8fb7dfc30a007a63349639632f4
Kenneth Karlsson <kenneth.karlsson.2@volvocars.com>
2018-05-25 15:42:27 +0200

    Add password and userid to artifactory push
    No-Jira
    
    Change-Id: Ic6076719416a2cb4c1c8824abafd0bf4a52f5002

--- tools/ci/jenkins/sdk_host_library.sh
+++ tools/ci/jenkins/sdk_host_library.sh
@@ -4,4 +4,4 @@
 # This file is covered by LICENSE file in the root of this project
 
 # TODO, build and do host tests
-"$ZUUL_PROJECT"/gradlew artifactoryPublish
+"$ZUUL_PROJECT"/gradlew artifactoryPublish -Partifactory_user=jenkins-icup_android -Partifactory_password="$ARTIFACTORY_API_KEY" -Partifactory_contextUrl=https://swf1.artifactory.cm.volvocars.biz/artifactory
