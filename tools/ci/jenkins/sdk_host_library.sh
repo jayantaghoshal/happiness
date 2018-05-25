db3856ca097dc011db445e24bfadfdc8d06f4c75
Kenneth Karlsson <kenneth.karlsson.2@volvocars.com>
2018-05-25 16:31:27 +0200

    Add some print outs
    No-Jira
    
    Change-Id: Ia3124e0a965104699191bf1211516b9f59641ee5

--- tools/ci/jenkins/sdk_host_library.sh
+++ tools/ci/jenkins/sdk_host_library.sh
@@ -4,4 +4,7 @@
 # This file is covered by LICENSE file in the root of this project
 
 # TODO, build and do host tests
+pwd
+ls -l
+echo "$ZUUL_PROJECT"
 "$ZUUL_PROJECT"/gradlew artifactoryPublish -Partifactory_user=jenkins-icup_android -Partifactory_password="$ARTIFACTORY_API_KEY" -Partifactory_contextUrl=https://swf1.artifactory.cm.volvocars.biz/artifactory
