881bdefb6b948c9eb94a9e37361d0e3982da6193
Kenneth Karlsson <kenneth.karlsson.2@volvocars.com>
2018-05-25 16:56:52 +0200

    Add printouts and show commands in shell
    No-Jira
    
    Change-Id: Ibf655a09c2fad7d6670117cdfb14301fd365840f

--- tools/ci/jenkins/sdk_host_library.sh
+++ tools/ci/jenkins/sdk_host_library.sh
@@ -4,7 +4,13 @@
 # This file is covered by LICENSE file in the root of this project
 
 # TODO, build and do host tests
+set -uex
 pwd
 ls -l
 echo "$ZUUL_PROJECT"
-"$ZUUL_PROJECT"/gradlew artifactoryPublish -Partifactory_user=jenkins-icup_android -Partifactory_password="$ARTIFACTORY_API_KEY" -Partifactory_contextUrl=https://swf1.artifactory.cm.volvocars.biz/artifactory
+
+"$ZUUL_PROJECT"/gradlew tasks
+"$ZUUL_PROJECT"/gradlew tasks -Partifactory_user=kkarls28 -Partifactory_password=AKCp5aTGrS6sFjkDahJ4xHTXSPg13UKKkXW1zam8b3WMitn3jFsUwYewCLTnikJk8v8gecuSZ -Partifactory_contextUrl=https://swf1.artifactory.cm.volvocars.biz/artifactory
+
+#"$ZUUL_PROJECT"/gradlew artifactoryPublish -Partifactory_user=jenkins-icup_android -Partifactory_password="$ARTIFACTORY_API_KEY" -Partifactory_contextUrl=https://swf1.artifactory.cm.volvocars.biz/artifactory
+"$ZUUL_PROJECT"/gradlew artifactoryPublish -Partifactory_user=kkarls28 -Partifactory_password=AKCp5aTGrS6sFjkDahJ4xHTXSPg13UKKkXW1zam8b3WMitn3jFsUwYewCLTnikJk8v8gecuSZ -Partifactory_contextUrl=https://swf1.artifactory.cm.volvocars.biz/artifactory
