71174b7666b869018bd5ee7c3de994a34accbcb9
Kenneth Karlsson <kenneth.karlsson.2@volvocars.com>
2018-05-28 13:05:05 +0200

    Refactor the SDK gate stage
    No-Jira
    
    Change-Id: Ie39fc0cd7f9eaecad183b6a8a0829dd44d58aa3a

--- tools/ci/jenkins/commit_check_hmi.sh
+++ tools/ci/jenkins/commit_check_hmi.sh
@@ -4,32 +4,6 @@
 # This file is covered by LICENSE file in the root of this project
 
 set -ex
-GIT_SSH="$HOME"/zuul_ssh_wrapper.sh zuul-cloner -v "${ZUUL_URL}" "${ZUUL_PROJECT}"
-# Download the Android SDK Command Line Tools
-# Version reference: https://developer.android.com/studio/
-if [ -d "android-sdk-linux" ]; then
-  echo "SDK already downloaded."
-else
-  wget https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip
-  unzip sdk-tools-linux-3859397.zip -d android-sdk-linux
-fi
-
-# Download the Android SDK
-export ANDROID_HOME="$PWD/android-sdk-linux"
-export PATH="$ANDROID_HOME"/tools:$ANDROID_HOME/platform-tools:$PATH
-
-"$ANDROID_HOME"/tools/bin/sdkmanager --update
-"$ANDROID_HOME"/tools/bin/sdkmanager "platforms;android-27" "build-tools;27.0.3" "extras;google;m2repository" "extras;android;m2repository"
-yes | "$ANDROID_HOME"/tools/bin/sdkmanager --licenses
-"$ANDROID_HOME"/tools/bin/sdkmanager "platforms;android-P" "build-tools;28.0.0-rc1" "extras;google;m2repository" "extras;android;m2repository"
-yes | "$ANDROID_HOME"/tools/bin/sdkmanager --licenses
-"$ANDROID_HOME"/tools/bin/sdkmanager --update
-yes | "$ANDROID_HOME"/tools/bin/sdkmanager --licenses
-
-# Set the SDK dir for Gradle
-#if ! grep -q "sdk.dir=$ANDROID_HOME" "$ZUUL_PROJECT/local.properties"; then
-#  echo sdk.dir="$ANDROID_HOME" >> "$ZUUL_PROJECT/local.properties"
-#fi
 
 # TODO: Run in sandbox and verify the output folder location and archive/result storage
 "$ZUUL_PROJECT"/gradlew clean assemble check -p "$ZUUL_PROJECT"
