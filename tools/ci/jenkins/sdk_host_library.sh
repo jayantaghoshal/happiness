4dc730d54202312df2673694baf95cf8f0e5bd6b
Kenneth Karlsson <kenneth.karlsson.2@volvocars.com>
2018-05-28 09:53:11 +0200

    Tweak the publish script
    No-Jira
    
    Change-Id: I1e83ceff27a167881472ee68b4050165d581c69b

--- tools/ci/jenkins/sdk_host_library.sh
+++ tools/ci/jenkins/sdk_host_library.sh
@@ -10,4 +10,4 @@ ls -l
 echo "$ZUUL_PROJECT"
 export ANDROID_HOME="$PWD/android-sdk-linux"
 
-"$ZUUL_PROJECT"/gradlew artifactoryPublish -Partifactory_user=kkarls28 -Partifactory_password=AKCp5aTGrS6sFjkDahJ4xHTXSPg13UKKkXW1zam8b3WMitn3jFsUwYewCLTnikJk8v8gecuSZ -Partifactory_contextUrl=https://swf1.artifactory.cm.volvocars.biz/artifactory
+"$ZUUL_PROJECT"/gradlew assemble artifactoryPublish -Partifactory_user=kkarls28 -Partifactory_password=AKCp5aTGrS6sFjkDahJ4xHTXSPg13UKKkXW1zam8b3WMitn3jFsUwYewCLTnikJk8v8gecuSZ -Partifactory_contextUrl=https://swf1.artifactory.cm.volvocars.biz/artifactory
