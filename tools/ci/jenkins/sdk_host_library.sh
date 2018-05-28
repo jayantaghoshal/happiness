33c6b9e17051399cb69892ea7406168ed051f51f
Kenneth Karlsson <kenneth.karlsson.2@volvocars.com>
2018-05-28 09:40:20 +0200

    Clean up of the library publish script
    No-Jira
    
    Change-Id: I21b862eb8f752557032ba97200cf532f64dcda31

--- tools/ci/jenkins/sdk_host_library.sh
+++ tools/ci/jenkins/sdk_host_library.sh
@@ -10,10 +10,4 @@ ls -l
 echo "$ZUUL_PROJECT"
 export ANDROID_HOME="$PWD/android-sdk-linux"
 
-cat "$ZUUL_PROJECT"/build.gradle
-"$ZUUL_PROJECT"/gradlew tasks --all
-"$ZUUL_PROJECT"/gradlew tasks -Partifactory_user=kkarls28 -Partifactory_password=AKCp5aTGrS6sFjkDahJ4xHTXSPg13UKKkXW1zam8b3WMitn3jFsUwYewCLTnikJk8v8gecuSZ -Partifactory_contextUrl=https://swf1.artifactory.cm.volvocars.biz/artifactory
-
-#"$ZUUL_PROJECT"/gradlew artifactoryPublish -Partifactory_user=jenkins-icup_android -Partifactory_password="$ARTIFACTORY_API_KEY" -Partifactory_contextUrl=https://swf1.artifactory.cm.volvocars.biz/artifactory
-"$ZUUL_PROJECT"/gradlew clean assemble check -p "$ZUUL_PROJECT" -Partifactory_user=kkarls28 -Partifactory_password=AKCp5aTGrS6sFjkDahJ4xHTXSPg13UKKkXW1zam8b3WMitn3jFsUwYewCLTnikJk8v8gecuSZ -Partifactory_contextUrl=https://swf1.artifactory.cm.volvocars.biz/artifactory
-"$ZUUL_PROJECT"/gradlew clean assemble artifactoryPublish -p "$ZUUL_PROJECT" -Partifactory_user=kkarls28 -Partifactory_password=AKCp5aTGrS6sFjkDahJ4xHTXSPg13UKKkXW1zam8b3WMitn3jFsUwYewCLTnikJk8v8gecuSZ -Partifactory_contextUrl=https://swf1.artifactory.cm.volvocars.biz/artifactory
+"$ZUUL_PROJECT"/gradlew artifactoryPublish -Partifactory_user=kkarls28 -Partifactory_password=AKCp5aTGrS6sFjkDahJ4xHTXSPg13UKKkXW1zam8b3WMitn3jFsUwYewCLTnikJk8v8gecuSZ -Partifactory_contextUrl=https://swf1.artifactory.cm.volvocars.biz/artifactory
