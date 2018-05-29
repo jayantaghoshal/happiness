1d9739dea7a6b971bb04b5c2d151a1587c4a7df3
HUI <hui.zhou@volvocars.com>
2018-05-29 15:41:37 +0000

    Fix daily kpi registry
    NO-JIRA
    
    Change-Id: Ia2589aca0e07595f6a9541f2993119efeda94280

--- tools/ci/shipit/handle_result/ci_database_reporter.py
+++ tools/ci/shipit/handle_result/ci_database_reporter.py
@@ -25,115 +25,13 @@ class ci_database_reporter(abstract_reporter):
 
     def plan_finished(self, test_results: List[ResultData]) -> None:
         #InfluxDB:
-        failing_testcases = [x for x in test_results if not x.passed]
-        passing_testcases = len(test_results) - len(failing_testcases)
-        # NOTE: Gate test does not have top job name and build number yet, hard code the value to align with hourly and daily
-        if "TOP_JOB_NUMBER" in os.environ and "TOP_JOB_JOBNAME" in os.environ and os.environ["TOP_JOB_NUMBER"] and os.environ["TOP_JOB_JOBNAME"]:
-            top_job_jobname = os.environ["TOP_JOB_JOBNAME"]
-            top_job_build_number = int(os.environ["TOP_JOB_NUMBER"])
-        else:
-            if os.environ["JOB_NAME"] == "ihu_gate_test":
-                top_job_jobname = "ihu_gate"
-            else:
-                top_job_jobname = ""
-            top_job_build_number = 0
-        data = {
-            'build_number' : os.environ["BUILD_NUMBER"],
-            'gerrit_id' : os.environ["ZUUL_CHANGE"],
-            'build_host' : os.environ["HOST_HOSTNAME"],
-            'job_name' : os.environ["JOB_NAME"],
-            'top_job_name': top_job_jobname,
-            'top_job_build_number' : top_job_build_number,
-            'passing_testcases' : passing_testcases,
-            'failing_testcases' : len(failing_testcases),
-            'total_testcases' : len(test_results),
-        }
-
-        tags = {
-            "jobType": "ihu_test",
-        }
-
-        logger.debug("Storing: {}".format(data))
-        try:
-            insert_influx_data(str(os.environ["JOB_NAME"]), data, tags)
-        except Exception as e:
-            logger.error(str(e))
-
-        kpidata = {
-            'build_number' : os.environ["BUILD_NUMBER"],
-            'build_host' : os.environ["HOST_HOSTNAME"],
-            'top_job_name': top_job_jobname,
-            'top_job_build_number' : top_job_build_number,
-        }
-
-        for r in test_results:
-            if r.name != 'VtsCiSmokeTest':
-                continue
-            for kpi in r.test_kpis:
-                kpidata[kpi] = r.test_kpis[kpi]
-
-        kpitags = {
-            "jobType": "ihu_test_kpi",
-        }
-
-        logger.debug("Storing: {}".format(data))
-        try:
-            insert_influx_data(str(os.environ["JOB_NAME"]) + "_kpi", kpidata, kpitags)
-        except Exception as e:
-            logger.error(str(e))
-
-
-        # create overview measurement for hourly and daily, since they are currently divided in seperated sections
-        self.regroup_data(passing_testcases, len(failing_testcases), len(test_results), top_job_jobname, top_job_build_number, top_job_jobname + "_overview")
+        self.test_overview_influx(test_results)
 
         #MongoDB:
         #link_template = "https://reports-ihu-ci.cm.volvocars.biz/tests?top_job_name={}&build_number={}"
         #link = link_template.format(top_job_jobname, top_job_build_number)
         link = "https://reports-ihu-ci.cm.volvocars.biz"
         logging.info("Results and logs can be accessed from: {}".format(link))
-
-    def regroup_data(self, passing_testcases, failing_testcases, total_testcases, top_job_jobname, top_job_build_number, measurement) -> None:
-        fieldPass = "passing_testcases"
-        fieldFail = "failing_testcases"
-        fieldTotal = "total_testcases"
-        fieldTime = "time"
-        build_number = "top_job_build_number={}".format(top_job_build_number)
-        try:
-            results = query_influx_data(fieldPass, fieldFail, fieldTotal, fieldTime, measurement, build_number)
-        except Exception as e:
-            logger.error(str(e))
-        points = results.get_points()
-        logger.debug("Result: {0}".format(results))
-
-        if len(results) > 0 :
-            logger.debug("Has record, append to existing measurement")
-            for item in points:
-                passing_testcases += item[fieldPass]
-                failing_testcases += item[fieldFail]
-                total_testcases += item[fieldTotal]
-                timestamp = item[fieldTime]
-        else:
-            logger.debug("No result yet, insert new measurement")
-            timestamp = ""
-
-        data = {
-            'gerrit_id' : os.environ["ZUUL_CHANGE"],
-            'top_job_name': top_job_jobname,
-            'top_job_build_number' : top_job_build_number,
-            'passing_testcases' : passing_testcases,
-            'failing_testcases' : failing_testcases,
-            'total_testcases' : total_testcases,
-        }
-        tags = {
-            "jobType": "ihu_test_overview",
-        }
-
-        logger.info("Storing: {}".format(data))
-        try:
-            insert_influx_data(measurement, data, tags, timestamp)
-        except Exception as e:
-            logger.error(str(e))
-
 
     def module_started(self, test: IhuBaseTest, testrun_uuid: str) -> None:
         if not self.continue_report:
@@ -226,3 +124,110 @@ class ci_database_reporter(abstract_reporter):
         logger.debug("Mongo data: {}".format(mongo_data))
 
         return mongo_data
+
+    def test_overview_influx(self, test_results: List[ResultData]) -> None:
+        failing_testcases = [x for x in test_results if not x.passed]
+        passing_testcases = len(test_results) - len(failing_testcases)
+        # NOTE: Gate test does not have top job name and build number yet, hard code the value to align with hourly and daily
+        if "TOP_JOB_NUMBER" in os.environ and "TOP_JOB_JOBNAME" in os.environ and os.environ["TOP_JOB_NUMBER"] and os.environ["TOP_JOB_JOBNAME"]:
+            top_job_jobname = os.environ["TOP_JOB_JOBNAME"]
+            top_job_build_number = int(os.environ["TOP_JOB_NUMBER"])
+        else:
+            if os.environ["JOB_NAME"] == "ihu_gate_test":
+                top_job_jobname = "ihu_gate"
+            else:
+                top_job_jobname = ""
+            top_job_build_number = 0
+        data = {
+            'build_number' : os.environ["BUILD_NUMBER"],
+            'gerrit_id' : os.environ["ZUUL_CHANGE"],
+            'build_host' : os.environ["HOST_HOSTNAME"],
+            'job_name' : os.environ["JOB_NAME"],
+            'top_job_name': top_job_jobname,
+            'top_job_build_number' : top_job_build_number,
+            'passing_testcases' : passing_testcases,
+            'failing_testcases' : len(failing_testcases),
+            'total_testcases' : len(test_results),
+        }
+
+        tags = {
+            "jobType": "ihu_test",
+        }
+
+        logger.debug("Storing: {}".format(data))
+        try:
+            insert_influx_data(str(os.environ["JOB_NAME"]), data, tags)
+        except Exception as e:
+            logger.error(str(e))
+
+        kpidata = {
+            'build_number' : os.environ["BUILD_NUMBER"],
+            'build_host' : os.environ["HOST_HOSTNAME"],
+            'top_job_name': top_job_jobname,
+            'top_job_build_number' : top_job_build_number,
+        }
+
+        for r in test_results:
+            if r.name != 'VtsCiSmokeTest':
+                continue
+            for kpi in r.test_kpis:
+                try:
+                    float(r.test_kpis[kpi])
+                    kpidata[kpi] = r.test_kpis[kpi]
+                except Exception:
+                    kpidata[kpi] = str(r.test_kpis[kpi])
+
+        kpitags = {
+            "jobType": "ihu_test_kpi",
+        }
+
+        logger.debug("Storing: {}".format(data))
+        try:
+            insert_influx_data(str(os.environ["JOB_NAME"]) + "_kpi", kpidata, kpitags)
+        except Exception as e:
+            logger.error(str(e))
+
+        # create overview measurement for hourly and daily, since they are currently divided in seperated sections
+        self.regroup_data(passing_testcases, len(failing_testcases), len(test_results), top_job_jobname, top_job_build_number, top_job_jobname + "_overview")
+
+    def regroup_data(self, passing_testcases, failing_testcases, total_testcases, top_job_jobname, top_job_build_number, measurement) -> None:
+        fieldPass = "passing_testcases"
+        fieldFail = "failing_testcases"
+        fieldTotal = "total_testcases"
+        fieldTime = "time"
+        build_number = "top_job_build_number={}".format(top_job_build_number)
+        try:
+            results = query_influx_data(fieldPass, fieldFail, fieldTotal, fieldTime, measurement, build_number)
+        except Exception as e:
+            logger.error(str(e))
+        points = results.get_points()
+        logger.debug("Result: {0}".format(results))
+
+        if len(results) > 0 :
+            logger.debug("Has record, append to existing measurement")
+            for item in points:
+                passing_testcases += item[fieldPass]
+                failing_testcases += item[fieldFail]
+                total_testcases += item[fieldTotal]
+                timestamp = item[fieldTime]
+        else:
+            logger.debug("No result yet, insert new measurement")
+            timestamp = ""
+
+        data = {
+            'gerrit_id' : os.environ["ZUUL_CHANGE"],
+            'top_job_name': top_job_jobname,
+            'top_job_build_number' : top_job_build_number,
+            'passing_testcases' : passing_testcases,
+            'failing_testcases' : failing_testcases,
+            'total_testcases' : total_testcases,
+        }
+        tags = {
+            "jobType": "ihu_test_overview",
+        }
+
+        logger.info("Storing: {}".format(data))
+        try:
+            insert_influx_data(measurement, data, tags, timestamp)
+        except Exception as e:
+            logger.error(str(e))
