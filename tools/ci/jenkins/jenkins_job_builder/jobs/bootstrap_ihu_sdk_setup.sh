fb852d37f1420f177fa382fe7244d5294cbf5500
Kenneth Karlsson <kenneth.karlsson.2@volvocars.com>
2018-06-01 12:39:37 +0200

    Sync ComponentLibrary and MediaLibrary before the SDK_setup
    No-Jira
    
    Change-Id: If3abaaaff6c76ee4fd04fea4522daecc1aa54196

--- tools/ci/jenkins/jenkins_job_builder/jobs/bootstrap_ihu_sdk_setup.sh
+++ tools/ci/jenkins/jenkins_job_builder/jobs/bootstrap_ihu_sdk_setup.sh
@@ -3,4 +3,7 @@
 # Copyright 2017-2018 Volvo Car Corporation
 # This file is covered by LICENSE file in the root of this project
 
+bootstrap_docker_run "repo sync --no-clone-bundle --current-branch --force-sync --detach -q -j8 vendor/volvocars/hmi/MediaLibrary"
+bootstrap_docker_run "repo sync --no-clone-bundle --current-branch --force-sync --detach -q -j8 vendor/volvocars/hmi/ComponentLibrary"
+
 ./vendor/volvocars/tools/ci/jenkins/ci_docker_run.sh --multiuser ./vendor/volvocars/tools/ci/jenkins/sdk_setup.sh
