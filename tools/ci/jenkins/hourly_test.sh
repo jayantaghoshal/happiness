ae4fde6ae880cbef43a6a0cf6c743b0b0bedf128
Per Karlsson <per.karlsson.2@volvocars.com>
2018-06-04 11:16:38 +0200

    CI: Bugfix in hourly_test.sh
    Change-Id: I3856a2b24c08758161ad41bdc0c443a2d14ae889

--- tools/ci/jenkins/hourly_test.sh
+++ tools/ci/jenkins/hourly_test.sh
@@ -5,7 +5,7 @@
 
 # Parameter for selecting dryrun test - controls the need for repo sync, which is needed for an open change in Gerrit.
 # Default is ordinary hourly build.
-test_type=$1
+test_type="$1"
 
 SCRIPT_DIR=$(cd "$(dirname "$(readlink -f "$0")")"; pwd)
 set +x
